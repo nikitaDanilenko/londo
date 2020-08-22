2b339c883599a49fc8545a4b19e724004247c4ed
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-08-22 10:27:23 +0200

    #1 Feat: Finish tasks table, add dashboards and dashboard-project link table

--- priv/repo/migrations/20200821230803_initial_db_set_up.exs
+++ priv/repo/migrations/20200821230803_initial_db_set_up.exs
@@ -13,18 +13,35 @@ defmodule Londo.Repo.Migrations.InitialDbSetUp do
 
     create table(:projects, primary_key: false) do
       add :id, :uuid, primary_key: true
-      add :parent_project, references(:projects, type :uuid), null: true
+      add :parent_project, references(:projects, type: :uuid, on_delete: :delete_all), null: true
       add :name, :string, null: false
       add :description, :string, null: true
     end
 
     create table(:user_projects, primary_key: false) do
-      add :user_id, references(:users, type: uuid), primary_key: true
-      add :project_id, references(:projects), primary_key: true
+      add :user_id, references(:users, type: :uuid, on_delete: :delete_all), primary_key: true
+      add :project_id, references(:projects, type: :uuid, on_delete: :delete_all), primary_key: true
     end
 
     create table(:tasks, primary_key: false) do
       add :id, :uuid, primary_key: true
+      add :project_id, references(:projects, type: :uuid, on_delete: :delete_all), null: false
+      add :progress_made, :integer, null: false
+      add :progress_possible, :integer, null: false
+      add :progress_unit, :string, null: true
+      add :description, :string, null: false
+      add :display_kind, :string, null: false
+    end
+
+    create table(:dashboards, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :user_id, references(:users, type: :uuid, on_delete: :delete_all)
+      add :description, :string, null: false
+    end
+
+    create table(:dashboard_projects, primary_key: false) do
+      add :dashboard_id, references(:dashboard, type: :uuid, on_delete: :delete_all), primary_key: true
+      add :project_id, references(:projects, type: :uuid, on_delete: :delete_all), primary_key: true
     end
   end
 end
