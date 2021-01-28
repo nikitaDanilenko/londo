fb835652be971cef918882a3d4e4f6e6ef8bdc79
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-01-28 12:34:57 +0100

    Add simplified code generator for database classes

---
+++ app/db/CodeGenerator.scala
@@ -0,0 +1,47 @@
+package db
+
+import com.typesafe.config.Config
+import io.getquill.codegen.jdbc.ComposeableTraitsJdbcCodegen
+import io.getquill.codegen.model.{ NameParser, SnakeCaseNames }
+import org.postgresql.ds.PGSimpleDataSource
+import play.api.Configuration
+import play.api.inject.guice.GuiceApplicationBuilder
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, Future }
+
+object CodeGenerator {
+
+  private val injector = new GuiceApplicationBuilder().injector()
+
+  private val configuration = injector.instanceOf[Configuration]
+
+  private val dbConfig = configuration.get[Config]("db.default")
+
+  val dataSource = new PGSimpleDataSource()
+
+  dataSource.setUser(dbConfig.getString("username"))
+  dataSource.setPassword(dbConfig.getString("password"))
+
+  private val cg: ComposeableTraitsJdbcCodegen =
+    new ComposeableTraitsJdbcCodegen(
+      dataSource = dataSource,
+      packagePrefix = "db.models",
+      nestedTrait = true
+    ) {
+
+      override val nameParser: NameParser = new SnakeCaseNames {
+        override val generateQuerySchemas: Boolean = true
+      }
+
+    }
+
+  def main(args: Array[String]): Unit = {
+    val paths = Future.sequence(cg.writeFiles("app/db/models"))
+    val finished = Await.result(paths, Duration(10, TimeUnit.SECONDS))
+    pprint.log(finished)
+  }
+
+}
