1ae5f0b51ef1fd4f2c1584c38bd470927a57f55f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-02-24 17:00:24 +0100

    Adjust db context, add preliminary transactor to context

--- app/db/DbContext.scala
+++ app/db/DbContext.scala
@@ -1,14 +1,26 @@
 package db
 
-import com.typesafe.config.Config
+import cats.effect.{ ContextShift, IO }
+import doobie.quill.DoobieContext
+import doobie.util.transactor.Transactor
 import io.getquill.context.Context
-import io.getquill.{ PostgresAsyncContext, PostgresDialect, SnakeCase }
-import play.api.Configuration
+import io.getquill.{ PostgresDialect, SnakeCase }
 
 import javax.inject.{ Inject, Singleton }
 
 @Singleton
-class DbContext @Inject() (configuration: Configuration)
-    extends PostgresAsyncContext[SnakeCase](SnakeCase, configuration.get[Config]("quill.dataSource"))
+class DbContext @Inject() (dbConnection: DbConnection)
+    extends DoobieContext.Postgres(SnakeCase: SnakeCase)
     with PublicExtensions[PostgresDialect, SnakeCase]
-    with Context[PostgresDialect, SnakeCase]
+    with Context[PostgresDialect, SnakeCase] {
+
+  // TODO: Should the transactor be constructed here via injection?
+  def transactor(implicit contextShift: ContextShift[IO]): Transactor[IO] =
+    Transactor.fromDriverManager[IO](
+      driver = dbConnection.driver,
+      url = dbConnection.url,
+      user = dbConnection.userName,
+      pass = dbConnection.password
+    )
+
+}
