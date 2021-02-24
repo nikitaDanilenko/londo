621e06a938a12df3b9096c679a2d5c989e970249
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-02-24 12:41:26 +0100

    Fix naming strategy

--- app/db/DbContext.scala
+++ app/db/DbContext.scala
@@ -2,7 +2,7 @@ package db
 
 import com.typesafe.config.Config
 import io.getquill.context.Context
-import io.getquill.{ Literal, PostgresAsyncContext, PostgresDialect, SnakeCase }
+import io.getquill.{ PostgresAsyncContext, PostgresDialect, SnakeCase }
 import play.api.Configuration
 
 import javax.inject.{ Inject, Singleton }
@@ -10,5 +10,5 @@ import javax.inject.{ Inject, Singleton }
 @Singleton
 class DbContext @Inject() (configuration: Configuration)
     extends PostgresAsyncContext[SnakeCase](SnakeCase, configuration.get[Config]("quill.dataSource"))
-    with PublicExtensions[PostgresDialect, Literal]
-    with Context[PostgresDialect, Literal]
+    with PublicExtensions[PostgresDialect, SnakeCase]
+    with Context[PostgresDialect, SnakeCase]
