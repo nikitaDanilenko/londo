0363b96ed47cbd8219ba2d490d9d74c154564f7c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-02-24 17:03:05 +0100

    Add experimental setup for testing db connections

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,26 @@
 package controllers
 
+import cats.effect.{ ContextShift, IO }
+import db.DbContext
+import doobie.implicits._
 import play.api.mvc._
 
 import javax.inject._
+import scala.concurrent.ExecutionContext
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
   */
 @Singleton
-class HomeController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject() (
+    val controllerComponents: ControllerComponents,
+    dbContext: DbContext
+)(implicit
+    executionContext: ExecutionContext
+) extends BaseController {
+
+  implicit val cs: ContextShift[IO] = ContextShiftProvider.fromExecutionContext
 
   /**
     * Create an Action to render an HTML page.
@@ -19,8 +30,10 @@ class HomeController @Inject() (val controllerComponents: ControllerComponents) 
     * a path of `/`.
     */
   def index: Action[AnyContent] =
-    Action { implicit request: Request[AnyContent] =>
-      Ok("Ah, progress!")
+    Action.async { implicit request: Request[AnyContent] =>
+      import dbContext._
+      val result = run(PublicSchema.UserDao.query).transact(transactor).unsafeToFuture()
+      result.map(u => Ok(s"Ah, progress! $u"))
     }
 
 }
