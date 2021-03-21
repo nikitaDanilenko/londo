086ca2204f5c8d0e6e891a035ef09158e3855829
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-21 11:11:32 +0100

    Simplify user import

--- app/daos/UserDAO.scala
+++ app/daos/UserDAO.scala
@@ -1,7 +1,8 @@
 package daos
 
 import cats.effect.{ Async, ContextShift }
-import db.{ DbContext, models }
+import db.DbContext
+import db.models.User
 import doobie.implicits._
 import io.getquill.ActionReturning
 
@@ -13,20 +14,20 @@ class UserDAO @Inject() (val dbContext: DbContext) {
   import dbContext.PublicSchema.UserDao
   import dbContext._
 
-  def find[F[_]: Async: ContextShift](userId: UUID): F[Option[models.User]] =
+  def find[F[_]: Async: ContextShift](userId: UUID): F[Option[User]] =
     run(findAction(userId)).map(_.headOption).transact(transactor[F])
 
-  def findAll[F[_]: Async: ContextShift](userIds: Seq[UUID]): F[List[models.User]] =
+  def findAll[F[_]: Async: ContextShift](userIds: Seq[UUID]): F[List[User]] =
     run(findAllAction(userIds)).transact(transactor[F])
 
   def insert[F[_]: Async: ContextShift](
-      row: models.User
-  ): F[models.User] = run(insertAction(row)).transact(transactor[F])
+      row: User
+  ): F[User] = run(insertAction(row)).transact(transactor[F])
 
-  def insertAll[F[_]: Async: ContextShift](rows: Seq[models.User]): F[List[models.User]] =
+  def insertAll[F[_]: Async: ContextShift](rows: Seq[User]): F[List[User]] =
     run(insertAllAction(rows)).transact(transactor[F])
 
-  def delete[F[_]: Async: ContextShift](userId: UUID): F[models.User] =
+  def delete[F[_]: Async: ContextShift](userId: UUID): F[User] =
     run(deleteAction(userId)).transact(transactor[F])
 
   def deleteAll[F[_]: Async: ContextShift](userIds: Seq[UUID]): F[Long] =
@@ -44,14 +45,14 @@ class UserDAO @Inject() (val dbContext: DbContext) {
     }
   }
 
-  private def insertAction(row: models.User): Quoted[ActionReturning[models.User, models.User]] =
+  private def insertAction(row: User): Quoted[ActionReturning[User, User]] =
     quote {
       UserDao.query
         .insert(lift(row))
         .returning(x => x)
     }
 
-  private def insertAllAction(rows: Seq[models.User]) =
+  private def insertAllAction(rows: Seq[User]) =
     quote {
       liftQuery(rows).foreach(e => UserDao.query.insert(e).returning(x => x))
     }
