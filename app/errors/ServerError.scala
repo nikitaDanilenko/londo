ef588bd71ad722066ae645542e2f1a7d27e44240
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-26 23:32:43 +0100

    Make instantiation class public

--- app/errors/ServerError.scala
+++ app/errors/ServerError.scala
@@ -6,7 +6,9 @@ sealed trait ServerError {
 
 object ServerError {
 
-  private abstract class ServerErrorInstance(override val message: String) extends ServerError
+  type Or[A] = Either[ServerError, A]
+
+  abstract class ServerErrorInstance(override val message: String) extends ServerError
 
   object Login {
     case object Failure extends ServerErrorInstance("Invalid combination of user name and password.")
