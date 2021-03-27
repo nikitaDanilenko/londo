a2b9e93ba67a656a320bbc524833741cd93bf570
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-27 10:59:20 +0100

    Remove the explicit converter object, move conversion to companion

--- app/services/user/User.scala
+++ app/services/user/User.scala
@@ -12,3 +12,27 @@ case class User(
     settings: UserSettings,
     details: UserDetails
 )
+
+object User {
+
+  def fromRow(userRow: db.models.User, settings: UserSettings, details: UserDetails): User =
+    User(
+      id = UserId(userRow.id),
+      nickname = userRow.nickname,
+      email = userRow.nickname,
+      passwordSalt = userRow.passwordSalt,
+      passwordHash = userRow.passwordHash,
+      settings = settings,
+      details = details
+    )
+
+  def toRow(user: User): db.models.User =
+    db.models.User(
+      id = user.id.uuid,
+      nickname = user.nickname,
+      email = user.email,
+      passwordSalt = user.passwordSalt,
+      passwordHash = user.passwordHash
+    )
+
+}
