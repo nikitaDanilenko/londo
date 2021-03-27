ef5dc078b9ae90682a31644d81153f3e4a6a259c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-27 01:05:16 +0100

    Add default values

--- app/services/user/UserDetails.scala
+++ app/services/user/UserDetails.scala
@@ -8,3 +8,20 @@ case class UserDetails(
     lastName: Option[String],
     description: Option[String]
 )
+
+object UserDetails {
+
+  val default: UserDetails = UserDetails(
+    firstName = None,
+    lastName = None,
+    description = None
+  )
+
+  def fromRow(userDetailsRow: db.models.UserDetails): UserDetails =
+    UserDetails(
+      firstName = userDetailsRow.firstName,
+      lastName = userDetailsRow.lastName,
+      description = userDetailsRow.description
+    )
+
+}
--- app/services/user/UserSettings.scala
+++ app/services/user/UserSettings.scala
@@ -4,3 +4,16 @@ import io.circe.generic.JsonCodec
 
 @JsonCodec
 case class UserSettings(darkMode: Boolean)
+
+object UserSettings {
+
+  val default: UserSettings = UserSettings(
+    darkMode = false
+  )
+
+  def fromRow(userSettingsRow: db.models.UserSettings): UserSettings =
+    UserSettings(
+      darkMode = userSettingsRow.darkMode
+    )
+
+}
