9094379f1e558fd2bf2c0a26ba0e49790103aeb7
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-28 10:47:43 +0200

    Propagate execution context via GraphQLServices

--- app/graphql/HasGraphQLServices.scala
+++ app/graphql/HasGraphQLServices.scala
@@ -2,10 +2,14 @@ package graphql
 
 import cats.effect.IO
 
+import scala.concurrent.ExecutionContext
+
 trait HasGraphQLServices {
   protected def graphQLServices: GraphQLServices
 
   protected implicit lazy val ioImplicits: IOImplicits[IO] =
     IOImplicits.fromExecutionContext(graphQLServices.executionContext)
+
+  protected implicit lazy val executionContext: ExecutionContext = graphQLServices.executionContext
 
 }
