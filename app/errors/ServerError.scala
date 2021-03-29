143991e963e54c78e8df90bed419ac27ee20d26b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-29 10:51:50 +0200

    Add authentication errors

--- app/errors/ServerError.scala
+++ app/errors/ServerError.scala
@@ -14,4 +14,14 @@ object ServerError {
     case object Failure extends ServerErrorInstance("Invalid combination of user name and password.")
   }
 
+  object Authentication {
+
+    object Token {
+      case object Decoding extends ServerErrorInstance("Error decoding JWT: Format or signature is wrong")
+      case object Content extends ServerErrorInstance("Error parsing JWT content: Unexpected format")
+      case object Missing extends ServerErrorInstance("Missing JWT")
+    }
+
+  }
+
 }
