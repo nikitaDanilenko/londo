f38da3f5c116b8e3a966000e2d1177df83496074
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-29 10:41:46 +0200

    Bind DI instance

--- app/modules/ApplicationModule.scala
+++ app/modules/ApplicationModule.scala
@@ -3,13 +3,20 @@ package modules
 import db.DbConnection
 import play.api.inject.Binding
 import play.api.{ Configuration, Environment }
+import utils.jwt.JwtConfiguration
 
 class ApplicationModule extends play.api.inject.Module {
 
   override def bindings(environment: Environment, configuration: Configuration): collection.Seq[Binding[_]] = {
-    Seq(
+    val settings = Seq(
       bind[DbConnection].toInstance(configuration.get[DbConnection]("db.default"))
     )
+
+    val configurations = Seq(
+      bind[JwtConfiguration].toInstance(JwtConfiguration(configuration))
+    )
+
+    List(settings, configurations).flatten
   }
 
 }
