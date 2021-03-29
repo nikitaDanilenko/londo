04356b18a56ef83e8ac7bb25c53d3fede5bee7fa
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-29 10:51:30 +0200

    Extend graphQL context with optional userId

--- app/graphql/GraphQLContext.scala
+++ app/graphql/GraphQLContext.scala
@@ -1,16 +1,32 @@
 package graphql
 
+import java.util.UUID
+
 sealed trait GraphQLContext {
   def mutation: Mutation
   def query: Query
+  def userId: Option[UUID]
 }
 
 object GraphQLContext {
 
-  def apply(graphQLServices: GraphQLServices): GraphQLContext =
+  def withoutUser(graphQLServices: GraphQLServices): GraphQLContext =
+    create(
+      graphQLServices = graphQLServices,
+      _userId = None
+    )
+
+  def withUser(graphQLServices: GraphQLServices, userId: UUID): GraphQLContext =
+    create(
+      graphQLServices = graphQLServices,
+      _userId = Some(userId)
+    )
+
+  private def create(graphQLServices: GraphQLServices, _userId: Option[UUID]): GraphQLContext =
     new GraphQLContext {
       override val mutation: Mutation = Mutation(graphQLServices)
       override val query: Query = Query(graphQLServices)
+      override val userId: Option[UUID] = _userId
     }
 
 }
