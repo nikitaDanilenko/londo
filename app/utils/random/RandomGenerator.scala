e47c613f638e1d71419a55c2c388968da4e60ce3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-29 18:15:52 +0200

    Switch to printable characters only

--- app/utils/random/RandomGenerator.scala
+++ app/utils/random/RandomGenerator.scala
@@ -8,7 +8,7 @@ import scala.util.Random
 object RandomGenerator {
 
   def randomString(length: Int): IO[String] =
-    IO(Random.nextString(length))
+    IO(1.to(length).map(_ => Random.nextPrintableChar()).mkString)
 
   def randomUUID: IO[UUID] =
     IO(UUID.randomUUID())
