d65b42340c8a47f76ee946e7ba66fa11bed67fda
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-29 14:02:35 +0200

    Add syntax for wrapping to server errors

--- app/graphql/HasGraphQLServices.scala
+++ app/graphql/HasGraphQLServices.scala
@@ -1,8 +1,9 @@
 package graphql
 
 import cats.effect.IO
+import errors.{ ServerError, ServerException }
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ ExecutionContext, Future }
 
 trait HasGraphQLServices {
   protected def graphQLServices: GraphQLServices
@@ -11,5 +12,25 @@ trait HasGraphQLServices {
     IOImplicits.fromExecutionContext(graphQLServices.executionContext)
 
   protected implicit lazy val executionContext: ExecutionContext = graphQLServices.executionContext
+
+}
+
+object HasGraphQLServices {
+
+  object syntax {
+
+    implicit class ToServerException[A](val future: Future[ServerError.Or[A]]) extends AnyVal {
+
+      def handleServerError(implicit executionContext: ExecutionContext): Future[A] =
+        future.flatMap(
+          _.fold(
+            serverError => Future.failed(ServerException(serverError)),
+            Future.successful
+          )
+        )
+
+    }
+
+  }
 
 }
