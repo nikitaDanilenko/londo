0860ec4bcdcf4fa97696535660bdeed03f7fcba5
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-03-30 10:35:35 +0200

    Extend random generator and switch length type to natural numbers

--- app/utils/random/RandomGenerator.scala
+++ app/utils/random/RandomGenerator.scala
@@ -1,14 +1,29 @@
 package utils.random
 
 import cats.effect.IO
+import spire.math.Natural
 
 import java.util.UUID
 import scala.util.Random
 
 object RandomGenerator {
 
-  def randomString(length: Int): IO[String] =
-    IO(1.to(length).map(_ => Random.nextPrintableChar()).mkString)
+  val alphaNumericCharacters: List[Char] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toList
+
+  def randomStringFrom(chars: List[Char], length: Natural): IO[String] =
+    IO {
+      val maxIndex = chars.length - 1
+      val charMap = chars.zipWithIndex.map(_.swap).toMap
+      1.until(length.intValue)
+        .map(_ => charMap(Random.nextInt(maxIndex)))
+        .mkString
+    }
+
+  def randomString(length: Natural): IO[String] =
+    IO(1.to(length.intValue).map(_ => Random.nextPrintableChar()).mkString)
+
+  def randomAlphaNumericString(length: Natural): IO[String] =
+    randomStringFrom(alphaNumericCharacters, length)
 
   def randomUUID: IO[UUID] =
     IO(UUID.randomUUID())
