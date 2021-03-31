c97d4d367ce146495eb8984b8711076f5b60156f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-01 00:55:29 +0200

    Add message digest function

--- app/security/Hash.scala
+++ app/security/Hash.scala
@@ -3,12 +3,14 @@ package security
 import services.user.PasswordParameters
 import spire.math.Natural
 
+import java.security.{ MessageDigest, Signature }
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
 object Hash {
 
   private val hashAlgorithm: String = "PBKDF2WithHmacSHA256"
+  private val signatureHashAlgorithm: String = "SHA3-512"
   private val keyLength: Int = 512
 
   def fromPassword(password: String, salt: String, iterations: Natural): String = {
@@ -22,5 +24,12 @@ object Hash {
 
   def verify(password: String, passwordParameters: PasswordParameters): Boolean =
     fromPassword(password, passwordParameters.salt, passwordParameters.iterations) == passwordParameters.hash
+
+  def messageDigest(string: String): String =
+    MessageDigest
+      .getInstance(signatureHashAlgorithm)
+      .digest(string.getBytes)
+      .map("%02x".format(_))
+      .mkString
 
 }
