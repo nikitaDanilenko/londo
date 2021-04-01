6c2dc522bb7040d5b0831344c5f16ccad36f786c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-01 11:38:19 +0200

    Add syntax extensions and switch to base64 encoding entirely

--- app/utils/string/StringUtil.scala
+++ app/utils/string/StringUtil.scala
@@ -4,13 +4,22 @@ import java.util.Base64
 
 object StringUtil {
 
-  def encodeBase64(string: String): String =
-    toBase64String(string.getBytes)
-
   def toBase64String(bytes: Array[Byte]): String =
-    new String(Base64.getEncoder.encode(bytes))
+    Base64.getEncoder.encodeToString(bytes)
 
   def fromBase64String(string: String): Array[Byte] =
     Base64.getDecoder.decode(string)
+
+  object syntax {
+
+    implicit class ByteArrayToBase64String(val bytes: Array[Byte]) extends AnyVal {
+      def asBase64String: String = toBase64String(bytes)
+    }
+
+    implicit class StringAsBase64ByteArray(val string: String) extends AnyVal {
+      def asBase64ByteArray: Array[Byte] = fromBase64String(string)
+    }
+
+  }
 
 }
