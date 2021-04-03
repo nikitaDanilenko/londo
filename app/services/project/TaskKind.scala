dcab147325a19f9da7f7d17afb0e88c18c7a8e97
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-03 12:05:34 +0200

    #7: Rename task kind for symmetry

--- app/services/project/TaskKind.scala
+++ app/services/project/TaskKind.scala
@@ -5,5 +5,5 @@ sealed trait TaskKind
 object TaskKind {
   case object Discrete extends TaskKind
   case object Percentual extends TaskKind
-  case object Fraction extends TaskKind
+  case object Fractional extends TaskKind
 }
