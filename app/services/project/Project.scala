e26d3b64821390115e75aad83f14ebad6195f8c3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-05 10:57:49 +0200

    #7: Extend project structure by accessors, weight, and parent

--- app/services/project/Project.scala
+++ app/services/project/Project.scala
@@ -8,5 +8,52 @@ case class Project(
     subProjects: Vector[Project],
     name: String,
     description: Option[String],
-    ownerId: UserId
+    ownerId: UserId,
+    parentProjectId: Option[ProjectId],
+    weight: Int,
+    readAccessors: Accessors,
+    writeAccessors: Accessors
 )
+
+object Project {
+
+  def toRow(project: Project): DbComponents =
+    DbComponents(
+      project = db.models.Project(
+        id = project.id.uuid,
+        ownerId = project.ownerId.uuid,
+        name = project.name,
+        description = project.description,
+        parentProjectId = project.parentProjectId.map(_.uuid),
+        weight = project.weight,
+        isReadRestricted = Accessors.isRestricted(project.readAccessors),
+        isWriteRestricted = Accessors.isRestricted(project.writeAccessors)
+      ),
+      accessors =
+        accessorsWith(
+          writeAllowed = false,
+          accessors = project.readAccessors,
+          projectId = project.id
+        ) ++
+          accessorsWith(
+            writeAllowed = true,
+            accessors = project.writeAccessors,
+            projectId = project.id
+          )
+    )
+
+  private def accessorsWith(
+      writeAllowed: Boolean,
+      accessors: Accessors,
+      projectId: ProjectId
+  ): Set[db.models.ProjectAccess] =
+    Accessors
+      .userIdsOf(accessors)
+      .map(userId => db.models.ProjectAccess(projectId.uuid, userId.uuid, writeAllowed = writeAllowed))
+
+  case class DbComponents(
+      project: db.models.Project,
+      accessors: Set[db.models.ProjectAccess]
+  )
+
+}
