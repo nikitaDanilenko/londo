4d7375559e27952878732c68fb5e3f6455912d6c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-05 12:00:49 +0200

    #7: Add task conversions

--- app/services/project/Task.scala
+++ app/services/project/Task.scala
@@ -1,10 +1,46 @@
 package services.project
 
+import spire.math.Natural
+
 case class Task(
     id: TaskId,
     projectId: ProjectId,
     name: String,
     unit: Option[String],
     kind: TaskKind,
-    progress: Progress
+    progress: Progress,
+    weight: Int
 )
+
+object Task {
+
+  def fromRow(taskRow: db.models.Task): Task =
+    Task(
+      id = TaskId(taskRow.id),
+      projectId = ProjectId(taskRow.projectId),
+      name = taskRow.name,
+      unit = taskRow.unit,
+      kind = TaskKind.fromId(taskRow.kindId),
+      progress = Progress.zero(asNatural(taskRow.reachable)).set(asNatural(taskRow.reached)),
+      weight = taskRow.weight
+    )
+
+  def toRow(task: Task): db.models.Task =
+    db.models.Task(
+      id = task.id.uuid,
+      projectId = task.projectId.uuid,
+      name = task.name,
+      unit = task.unit,
+      kindId = TaskKind.toRow(task.kind).id,
+      reached = asBigDecimal(task.progress.reached),
+      reachable = asBigDecimal(task.progress.reachable),
+      weight = task.weight
+    )
+
+  private def asNatural(bigDecimal: BigDecimal): Natural =
+    Natural(bigDecimal.toBigInt)
+
+  private def asBigDecimal(natural: Natural): BigDecimal =
+    BigDecimal(natural.toBigInt)
+
+}
