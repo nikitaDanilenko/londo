5892a46dda666e2327dcdfdff90b47c2a596453e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-12 21:53:41 +0200

    #7: Add server errors for task validation

--- app/errors/ServerError.scala
+++ app/errors/ServerError.scala
@@ -1,5 +1,6 @@
 package errors
 
+import cats.data.{ NonEmptyList, Validated, ValidatedNel }
 import io.circe.{ Encoder, Json }
 import io.circe.syntax._
 
@@ -10,6 +11,18 @@ sealed trait ServerError {
 object ServerError {
 
   type Or[A] = Either[ServerError, A]
+  type Valid[A] = ValidatedNel[ServerError, A]
+
+  def fromEmpty[A](option: Option[A], ifNonEmpty: => ServerError): Valid[Unit] =
+    Validated.fromEither {
+      option match {
+        case Some(_) => Left(NonEmptyList.of(ifNonEmpty))
+        case None    => Right(())
+      }
+    }
+
+  def fromEither[A](either: Or[A]): Valid[A] =
+    Validated.fromEither[NonEmptyList[ServerError], A](either.left.map(NonEmptyList.of(_)))
 
   implicit val serverErrorEncoder: Encoder[ServerError] = Encoder.instance[ServerError] { serverError =>
     Json.obj(
@@ -44,6 +57,29 @@ object ServerError {
 
   object User {
     case object NotFound extends ServerErrorInstance("No user with the given id found")
+  }
+
+  object Task {
+
+    object Plain {
+      case object EmptyName extends ServerErrorInstance("Empty name in plain task")
+      case object EmptyKind extends ServerErrorInstance("Empty kind in plain task")
+      case object EmptyReached extends ServerErrorInstance("Empty reached value in plain task")
+      case object EmptyReachable extends ServerErrorInstance("Empty reachable value in plain task")
+      case object EmptyWeight extends ServerErrorInstance("Empty weight in plain task")
+      case object NonEmptyProjectReference extends ServerErrorInstance("Non-empty project reference in plain task")
+    }
+
+    object ProjectReference {
+      case object NonEmptyName extends ServerErrorInstance("Non-empty name in project reference task")
+      case object NonEmptyKind extends ServerErrorInstance("Non-empty kind in project reference task")
+      case object NonEmptyReached extends ServerErrorInstance("Non-empty reached value in project reference task")
+      case object NonEmptyReachable extends ServerErrorInstance("Non-empty reachable value in project reference task")
+      case object NonEmptyWeight extends ServerErrorInstance("Non-empty weight in project reference task")
+      case object NonEmptyUnit extends ServerErrorInstance("Non-empty unit in project reference task")
+      case object EmptyProjectReference extends ServerErrorInstance("Empty project reference in project reference task")
+    }
+
   }
 
 }
