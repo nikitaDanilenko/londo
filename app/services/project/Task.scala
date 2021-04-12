4a2f313dd5dbaf873b18e27a8bc168ad104e9a1c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-12 21:56:31 +0200

    #7: Add conversion function for tasks from rows

--- app/services/project/Task.scala
+++ app/services/project/Task.scala
@@ -1,41 +1,85 @@
 package services.project
 
+import cats.syntax.contravariantSemigroupal._
+import errors.ServerError
 import spire.math.Natural
 
-case class Task(
-    id: TaskId,
-    projectId: ProjectId,
-    name: String,
-    unit: Option[String],
-    kind: TaskKind,
-    progress: Progress,
-    weight: Int
-)
+sealed trait Task {
+  def id: TaskId
+  def projectId: ProjectId
+}
 
 object Task {
 
-  def fromRow(taskRow: db.models.Task): Task =
-    Task(
-      id = TaskId(taskRow.id),
-      projectId = ProjectId(taskRow.projectId),
-      name = taskRow.name,
-      unit = taskRow.unit,
-      kind = TaskKind.fromId(taskRow.kindId),
-      progress = Progress.zero(asNatural(taskRow.reachable)).set(asNatural(taskRow.reached)),
-      weight = taskRow.weight
-    )
+  case class Plain(
+      override val id: TaskId,
+      override val projectId: ProjectId,
+      name: String,
+      taskKind: TaskKind,
+      unit: Option[String],
+      progress: Progress,
+      weight: Int
+  ) extends Task
 
-  def toRow(task: Task): db.models.Task =
-    db.models.Task(
-      id = task.id.uuid,
-      projectId = task.projectId.uuid,
-      name = task.name,
-      unit = task.unit,
-      kindId = TaskKind.toRow(task.kind).id,
-      reached = asBigDecimal(task.progress.reached),
-      reachable = asBigDecimal(task.progress.reachable),
-      weight = task.weight
-    )
+  case class ProjectReference(
+      override val id: TaskId,
+      override val projectId: ProjectId,
+      projectReference: ProjectId
+  ) extends Task
+
+  def fromRow(taskRow: db.models.Task): ServerError.Valid[Task] = {
+
+    val plain = (
+      ServerError.fromEmpty(taskRow.projectReferenceId, ServerError.Task.Plain.NonEmptyProjectReference),
+      ServerError.fromEither(taskRow.name.toRight(ServerError.Task.Plain.EmptyName)),
+      ServerError.fromEither(taskRow.kindId.map(TaskKind.fromId).toRight(ServerError.Task.Plain.EmptyKind)),
+      ServerError.fromEither(taskRow.reachable.toRight(ServerError.Task.Plain.EmptyReachable)),
+      ServerError.fromEither(taskRow.reached.toRight(ServerError.Task.Plain.EmptyReached)),
+      ServerError.fromEither(taskRow.weight.toRight(ServerError.Task.Plain.EmptyWeight))
+    ).mapN { (_, name, taskKind, reachable, reached, weight) =>
+      Plain(
+        id = TaskId(taskRow.id),
+        projectId = ProjectId(taskRow.projectId),
+        name = name,
+        taskKind = taskKind,
+        unit = taskRow.unit,
+        progress = Progress.fraction(asNatural(reachable), asNatural(reached)),
+        weight = weight
+      )
+    }
+
+    val projectReference = (
+      ServerError.fromEither(
+        taskRow.projectReferenceId.toRight(ServerError.Task.ProjectReference.EmptyProjectReference)
+      ),
+      ServerError.fromEmpty(taskRow.name, ServerError.Task.ProjectReference.NonEmptyName),
+      ServerError.fromEmpty(taskRow.kindId, ServerError.Task.ProjectReference.NonEmptyKind),
+      ServerError.fromEmpty(taskRow.reachable, ServerError.Task.ProjectReference.NonEmptyReachable),
+      ServerError.fromEmpty(taskRow.reached, ServerError.Task.ProjectReference.NonEmptyReached),
+      ServerError.fromEmpty(taskRow.weight, ServerError.Task.ProjectReference.NonEmptyWeight),
+      ServerError.fromEmpty(taskRow.unit, ServerError.Task.ProjectReference.NonEmptyUnit)
+    ).mapN { (projectReferenceId, _, _, _, _, _, _) =>
+      ProjectReference(
+        id = TaskId(taskRow.id),
+        projectId = ProjectId(taskRow.projectId),
+        ProjectId(projectReferenceId)
+      )
+    }
+
+    plain.findValid(projectReference)
+  }
+
+  def toRow(task: Task): db.models.Task = ???
+//    db.models.Task(
+//      id = task.id.uuid,
+//      projectId = task.projectId.uuid,
+//      name = task.name,
+//      unit = task.unit,
+//      kindId = TaskKind.toRow(task.kind).id,
+//      reached = asBigDecimal(task.progress.reached),
+//      reachable = asBigDecimal(task.progress.reachable),
+//      weight = task.weight
+//    )
 
   private def asNatural(bigDecimal: BigDecimal): Natural =
     Natural(bigDecimal.toBigInt)
