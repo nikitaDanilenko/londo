4a4033a2f44ef4cfefa936259bd243119839664b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-22 00:19:06 +0200

    #7: Add conversion function for project ids

--- app/db/keys/ProjectId.scala
+++ app/db/keys/ProjectId.scala
@@ -5,4 +5,7 @@ import io.circe.generic.JsonCodec
 import java.util.UUID
 
 @JsonCodec
-case class ProjectId(uuid: UUID) extends AnyVal
+case class ProjectId(uuid: UUID) extends AnyVal {
+  def asProjectReadAccessId: ProjectReadAccessId = ProjectReadAccessId(uuid)
+  def asProjectWriteAccessId: ProjectWriteAccessId = ProjectWriteAccessId(uuid)
+}
