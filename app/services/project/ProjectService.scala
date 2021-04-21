b7d4a86a164e01880a20cea04916e6571ad841e8
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-22 00:19:54 +0200

    #7: Continue implementation of access handling, use transaction pattern

--- app/services/project/ProjectService.scala
+++ app/services/project/ProjectService.scala
@@ -1,38 +1,31 @@
 package services.project
 
 import cats.effect.{ Async, ContextShift }
-import db.generated.daos.{
-  ProjectDAO,
-  ProjectReadAccessDAO,
-  ProjectReadAccessEntryDAO,
-  ProjectWriteAccessDAO,
-  ProjectWriteAccessEntryDAO
-}
-import cats.syntax.flatMap._
-import cats.syntax.functor._
-import AccessToDB.instances._
-import db.models.{ ProjectReadAccess, ProjectReadAccessEntry }
-import AccessFromDB.instances._
+import cats.syntax.contravariantSemigroupal._
 import db.DAOFunctions
-import db.DAOFunctions._
-import db.keys.{ ProjectId, UserId }
+import db.generated.daos._
+import db.keys.ProjectId
+import doobie.ConnectionIO
+import services.project.AccessFromDB.instances._
+import services.project.AccessToDB.instances._
 
 import javax.inject.Inject
 
 class ProjectService @Inject() (
     projectDAO: ProjectDAO,
-    implicit val projectReadAccessDAO: ProjectReadAccessDAO,
+    projectReadAccessDAO: ProjectReadAccessDAO,
     projectReadAccessEntryDAO: ProjectReadAccessEntryDAO,
     projectWriteAccessDAO: ProjectWriteAccessDAO,
     projectWriteAccessEntryDAO: ProjectWriteAccessEntryDAO
 ) {
 
   def create[F[_]: Async: ContextShift](projectCreation: ProjectCreation): F[Project] = {
-    for {
-      createdProject <- Async[F].liftIO(ProjectCreation.create(projectCreation))
+    val action = for {
+      createdProject <- Async[ConnectionIO].liftIO(ProjectCreation.create(projectCreation))
       dbComponents = Project.toRow(createdProject)
-      project <- projectDAO.insert(dbComponents.project)
+      project <- projectDAO.insertC(dbComponents.project)
       readAccess <- setReadAccess(createdProject.id, createdProject.readAccessors)
+      writeAccess <- setWriteAccess(createdProject.id, createdProject.writeAccessors)
     } yield project
     ???
   }
@@ -40,36 +33,41 @@ class ProjectService @Inject() (
   def setReadAccess[F[_]: Async: ContextShift](
       projectId: ProjectId,
       projectAccess: ProjectAccess[AccessKind.Read]
-  ): F[ProjectAccess[AccessKind.Read]] = {
-//    setAccess(projectId, projectAccess)
-    ???
-  }
+  ): ConnectionIO[ProjectAccess[AccessKind.Read]] =
+    setAccess(projectReadAccessDAO, projectReadAccessEntryDAO)(projectId, _.asProjectReadAccessId, projectAccess)
 
-  def setAccess[F[_], AccessK, DBAccessK, DBAccessEntry](
+  def setWriteAccess[F[_]: Async: ContextShift](
       projectId: ProjectId,
+      projectAccess: ProjectAccess[AccessKind.Write]
+  ): ConnectionIO[ProjectAccess[AccessKind.Write]] =
+    setAccess(projectWriteAccessDAO, projectWriteAccessEntryDAO)(projectId, _.asProjectWriteAccessId, projectAccess)
+
+  private def setAccess[F[_], AccessK, DBAccessK, DBAccessKey, DBAccessEntry, DBAccessEntryKey](
+      daoFunctionsDBAccessK: DAOFunctions[DBAccessK, DBAccessKey],
+      daoFunctionsDBAccessEntry: DAOFunctions[DBAccessEntry, DBAccessEntryKey]
+  )(
+      projectId: ProjectId,
+      accessIdOf: ProjectId => DBAccessKey,
       projectAccess: ProjectAccess[AccessK]
   )(implicit
-      asyncF: Async[F],
-      contextShiftF: ContextShift[F],
       accessToDB: AccessToDB[AccessK, DBAccessK, DBAccessEntry],
-      accessFromDB: AccessFromDB[AccessK, DBAccessK, DBAccessEntry],
-      daoFunctionsDBAccessK: DAOFunctions[DBAccessK],
-      daoFunctionsDBAccessEntry: DAOFunctions[DBAccessEntry]
-  ): F[ProjectAccess[AccessK]] = {
-    val dbAction: F[Option[ProjectAccess.DbComponents[DBAccessK, DBAccessEntry]]] =
+      accessFromDB: AccessFromDB[AccessK, DBAccessK, DBAccessEntry]
+  ): ConnectionIO[ProjectAccess[AccessK]] = {
+    val dbAction: ConnectionIO[Option[ProjectAccess.DbComponents[DBAccessK, DBAccessEntry]]] =
       ProjectAccess.DbComponents(projectId, projectAccess) match {
         case Some(components) =>
-          for {
-            access <- daoFunctionsDBAccessK.insert(components.access)
-            entries <- daoFunctionsDBAccessEntry.insertAll(components.accessEntries)
-            // Todo: The type annotation should be unnecessary
-          } yield ProjectAccess.DbComponents[AccessK, DBAccessK, DBAccessEntry](
-            projectId = accessFromDB.projectId(access),
-            projectAccess = ProjectAccess(Accessors.restricted(accessFromDB.entryUserIds(access, entries)))
-          )
+          (
+            daoFunctionsDBAccessK.insertC(components.access),
+            daoFunctionsDBAccessEntry.insertAllC(components.accessEntries)
+          ).mapN { (access, entries) =>
+            ProjectAccess.DbComponents[AccessK, DBAccessK, DBAccessEntry](
+              projectId = accessFromDB.projectId(access),
+              projectAccess = ProjectAccess(Accessors.restricted(accessFromDB.entryUserIds(access, entries)))
+            )
+          }
         case None =>
-          projectReadAccessDAO
-            .delete(projectId.uuid)
+          daoFunctionsDBAccessK
+            .deleteC(accessIdOf(projectId))
             .map(_ => None: Option[ProjectAccess.DbComponents[DBAccessK, DBAccessEntry]])
       }
     dbAction.map(ProjectAccess.fromDb[AccessK, DBAccessK, DBAccessEntry])
