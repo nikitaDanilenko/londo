22988c6c319030998f867908f37de07329c9fae3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-23 00:26:55 +0200

    #7: Add fetch function, add preliminary delete function, adjust calls to new interface

--- app/services/project/ProjectService.scala
+++ app/services/project/ProjectService.scala
@@ -1,5 +1,6 @@
 package services.project
 
+import cats.data.EitherT
 import cats.effect.{ Async, ContextShift }
 import cats.syntax.contravariantSemigroupal._
 import db.generated.daos._
@@ -19,6 +20,7 @@ class ProjectService @Inject() (
     projectReadAccessEntryDAO: ProjectReadAccessEntryDAO,
     projectWriteAccessDAO: ProjectWriteAccessDAO,
     projectWriteAccessEntryDAO: ProjectWriteAccessEntryDAO,
+    taskDAO: TaskDAO,
     dbTransactorProvider: DbTransactorProvider
 ) {
 
@@ -31,7 +33,7 @@ class ProjectService @Inject() (
     } yield Project.fromRow(
       Project.DbComponents.fromComponents(
         project = project,
-        tasks = createdProject.tasks,
+        tasks = createdProject.tasks.map(Task.toRow),
         readAccessors = readAccessors,
         writeAccessors = writeAccessors
       )
@@ -82,9 +84,55 @@ class ProjectService @Inject() (
     dbAction.map(ProjectAccess.fromDb[AccessK, DBAccessK, DBAccessEntry])
   }
 
-  def delete = ???
+  // TODO: This function should return the complete old project
+  def delete[F[_]: Async: ContextShift](projectId: ProjectId): F[db.models.Project] = projectDAO.delete[F](projectId)
   def update = ???
-  def fetch = ???
+
+  def fetch[F[_]: Async: ContextShift](projectId: ProjectId): F[ServerError.Valid[Project]] = {
+    val projectReadAccessId = projectId.asProjectReadAccessId
+    val projectWriteAccessId = projectId.asProjectWriteAccessId
+
+    def liftF[A](a: ConnectionIO[A]): EitherT[ConnectionIO, ServerError, A] =
+      EitherT.liftF[ConnectionIO, ServerError, A](a)
+
+    val action = for {
+      projectRow <- EitherT.fromOptionF(projectDAO.findC(projectId), ServerError.Project.NotFound)
+      tasks <- liftF(taskDAO.findByProjectIdC(projectId.uuid))
+      readAccess <- liftF(projectReadAccessDAO.findC(projectReadAccessId))
+      readAccessEntries <- liftF(projectReadAccessEntryDAO.findByProjectReadAccessIdC(projectReadAccessId.uuid))
+      writeAccess <- liftF(projectWriteAccessDAO.findC(projectWriteAccessId))
+      writeAccessEntries <- liftF(projectWriteAccessEntryDAO.findByProjectWriteAccessIdC(projectWriteAccessId.uuid))
+    } yield {
+      Project.fromRow(
+        Project.DbComponents.fromComponents(
+          project = projectRow,
+          tasks = tasks,
+          readAccessors = ProjectAccess.fromDb(
+            readAccess.map(
+              ProjectAccess.DbComponents.fromComponents(
+                _,
+                readAccessEntries
+              )
+            )
+          ),
+          writeAccessors = ProjectAccess.fromDb(
+            writeAccess.map(
+              ProjectAccess.DbComponents.fromComponents(
+                _,
+                writeAccessEntries
+              )
+            )
+          )
+        )
+      )
+    }
+    action
+      .fold(
+        error => ServerError.fromEither[Project](Left(error)),
+        identity
+      )
+      .transact(dbTransactorProvider.transactor[F])
+  }
 
   def createTask = ???
   def removeTask = ???
