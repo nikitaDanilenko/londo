8f6b1c155e85d33c540117e761f80d21f23856c8
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-27 00:00:20 +0200

    #7: Add task creation type

---
+++ app/services/project/TaskCreation.scala
@@ -0,0 +1,49 @@
+package services.project
+
+import cats.effect.IO
+import db.keys.ProjectId
+import spire.math.Natural
+import utils.random.RandomGenerator
+
+sealed trait TaskCreation {
+  def weight: Natural
+}
+
+object TaskCreation {
+
+  case class Plain(
+      name: String,
+      taskKind: TaskKind,
+      unit: Option[String],
+      progress: Progress,
+      override val weight: Natural
+  ) extends TaskCreation
+
+  case class ProjectReference(
+      override val weight: Natural,
+      projectReferenceId: ProjectId
+  ) extends TaskCreation
+
+  def create(taskCreation: TaskCreation): IO[Task] =
+    RandomGenerator.randomUUID.map { uuid =>
+      val taskId = TaskId(uuid)
+      taskCreation match {
+        case Plain(name, taskKind, unit, progress, weight) =>
+          Task.Plain(
+            id = taskId,
+            name = name,
+            taskKind = taskKind,
+            unit = unit,
+            progress = progress,
+            weight = weight
+          )
+        case ProjectReference(weight, projectReferenceId) =>
+          Task.ProjectReference(
+            id = taskId,
+            projectReference = projectReferenceId,
+            weight = weight
+          )
+      }
+    }
+
+}
