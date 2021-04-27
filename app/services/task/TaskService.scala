834ae6bbcc445bee6c266569040dd730f30d9777
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-04-28 00:00:54 +0200

    #7: Add fetching action

--- app/services/task/TaskService.scala
+++ app/services/task/TaskService.scala
@@ -15,13 +15,34 @@ class TaskService @Inject() (taskDAO: TaskDAO, transactionally: Transactionally)
       projectId: ProjectId,
       taskCreation: TaskCreation
   ): F[ServerError.Valid[Task]] =
-    transactionally {
-      for {
-        createdTask <- Async[ConnectionIO].liftIO(TaskCreation.create(taskCreation))
-        taskRow = Task.toRow(projectId, createdTask)
-        writtenTask <- taskDAO.insertC(taskRow)
-      } yield Task.fromRow(writtenTask)
-    }
+    transactionally(createTaskC(projectId, taskCreation))
+
+  def createTaskC(
+      projectId: ProjectId,
+      taskCreation: TaskCreation
+  ): ConnectionIO[ServerError.Valid[Task]] =
+    for {
+      createdTask <- Async[ConnectionIO].liftIO(TaskCreation.create(taskCreation))
+      taskRow = Task.toRow(projectId, createdTask)
+      writtenTask <- taskDAO.insertC(taskRow)
+    } yield Task.fromRow(writtenTask)
+
+  def fetch[F[_]: Async: ContextShift](taskKey: TaskKey): F[ServerError.Valid[Task]] =
+    transactionally(fetchC(taskKey))
+
+  def fetchC(taskKey: TaskKey): ConnectionIO[ServerError.Valid[Task]] =
+    taskDAO
+      .findC(
+        db.keys.TaskId(
+          projectId = taskKey.projectId,
+          uuid = taskKey.taskId.uuid
+        )
+      )
+      .map(
+        ServerError
+          .fromOption(_, ServerError.Task.NotFound)
+          .andThen(Task.fromRow)
+      )
 
   def removeTask = ???
   def updateTask = ???
