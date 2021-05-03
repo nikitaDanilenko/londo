9d55f336b6d0949796ad6dcc65191ee3c27157f0
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-05-04 00:31:06 +0200

    #7: Add more variants of accessors, and account for conversions

--- app/services/project/Accessors.scala
+++ app/services/project/Accessors.scala
@@ -1,7 +1,11 @@
 package services.project
 
+import cats.data.NonEmptySet
 import db.keys.UserId
 import io.circe.{ Decoder, Encoder }
+
+import scala.collection.immutable.SortedSet
+import spire.compat._
 
 sealed trait Accessors
 
@@ -11,26 +15,45 @@ object Accessors {
 
   case object Nobody extends Accessors
 
-  case class Restricted(users: Set[UserId]) extends Accessors
+  case class EveryoneExcept(excluded: NonEmptySet[UserId]) extends Accessors
 
-  implicit val accessorsEncoder: Encoder[Accessors] = Encoder[Option[Seq[UserId]]].contramap(toRepresentation)
+  case class NobodyExcept(included: NonEmptySet[UserId]) extends Accessors
 
-  implicit val accessorsDecoder: Decoder[Accessors] = Decoder[Option[Seq[UserId]]].map(fromRepresentation)
+  case class Restricted(allowed: NonEmptySet[UserId], forbidden: NonEmptySet[UserId]) extends Accessors
 
-  def fromRepresentation(users: Option[Seq[UserId]]): Accessors =
+  implicit val accessorsEncoder: Encoder[Accessors] = Encoder[Option[UserRestriction]].contramap(toRepresentation)
+
+  implicit val accessorsDecoder: Decoder[Accessors] = Decoder[Option[UserRestriction]].map(fromRepresentation)
+
+  def fromRepresentation(users: Option[UserRestriction]): Accessors = {
+    def unsafeToNonEmptySet(userIds: Seq[UserId]): NonEmptySet[UserId] =
+      NonEmptySet.fromSetUnsafe(SortedSet.from(userIds))
+
     users match {
-      case Some(ids) =>
-        if (ids.isEmpty)
+      case Some(UserRestriction(allowed, forbidden)) =>
+        if (allowed.isEmpty && forbidden.isEmpty)
           Nobody
-        else Restricted(ids.toSet)
+        else if (allowed.isEmpty)
+          EveryoneExcept(unsafeToNonEmptySet(forbidden))
+        else if (forbidden.isEmpty)
+          NobodyExcept(unsafeToNonEmptySet(allowed))
+        else
+          Restricted(
+            allowed = unsafeToNonEmptySet(allowed),
+            forbidden = unsafeToNonEmptySet(forbidden)
+          )
       case None => Everyone
     }
+  }
 
-  def toRepresentation(accessors: Accessors): Option[Seq[UserId]] =
+  def toRepresentation(accessors: Accessors): Option[UserRestriction] =
     accessors match {
-      case Everyone          => None
-      case Nobody            => Some(Seq.empty)
-      case Restricted(users) => Some(users.toVector)
+      case Everyone                 => None
+      case Nobody                   => Some(UserRestriction.empty)
+      case EveryoneExcept(excluded) => Some(UserRestriction.excluded(excluded.toNonEmptyList.toList))
+      case NobodyExcept(excluded)   => Some(UserRestriction.included(excluded.toNonEmptyList.toList))
+      case Restricted(allowed, forbidden) =>
+        Some(UserRestriction(allowed = allowed.toNonEmptyList.toList, forbidden = forbidden.toNonEmptyList.toList))
     }
 
   def isRestricted(accessors: Accessors): Boolean =
@@ -39,13 +62,56 @@ object Accessors {
       case _        => true
     }
 
-  def userIdsOf(accessors: Accessors): Set[UserId] =
-    accessors match {
-      case Restricted(users) => users
-      case _                 => Set.empty
-    }
-
   def restricted(users: Set[UserId]): Accessors =
     if (users.isEmpty) Nobody else Restricted(users)
+
+  def allowUser(accessors: Accessors, userId: UserId): Accessors =
+    accessors match {
+      case Everyone => Everyone
+      case Nobody   => NobodyExcept(NonEmptySet.one(userId))
+      case EveryoneExcept(excluded) =>
+        NonEmptySet
+          .fromSet(excluded - userId)
+          .fold(Everyone: Accessors)(excludedWithoutUser =>
+            Restricted(
+              allowed = NonEmptySet.one(userId),
+              forbidden = excludedWithoutUser
+            )
+          )
+      case NobodyExcept(included) => NobodyExcept(included.add(userId))
+      case Restricted(allowed, forbidden) =>
+        val allowedWithUser = allowed.add(userId)
+        NonEmptySet
+          .fromSet(forbidden - userId)
+          .fold(NobodyExcept(allowedWithUser): Accessors)(forbiddenWithoutUser =>
+            Restricted(allowedWithUser, forbiddenWithoutUser)
+          )
+    }
+
+  def forbidUser(accessors: Accessors, userId: UserId): Accessors =
+    accessors match {
+      case Everyone                 => EveryoneExcept(NonEmptySet.one(userId))
+      case Nobody                   => Nobody
+      case EveryoneExcept(excluded) => EveryoneExcept(excluded.add(userId))
+      case NobodyExcept(included) =>
+        NonEmptySet
+          .fromSet(included - userId)
+          .fold(Nobody: Accessors)(includedWithoutUser =>
+            Restricted(
+              allowed = includedWithoutUser,
+              forbidden = NonEmptySet.one(userId)
+            )
+          )
+      case Restricted(allowed, forbidden) =>
+        val forbiddenWithUser = forbidden.add(userId)
+        NonEmptySet
+          .fromSet(allowed - userId)
+          .fold(EveryoneExcept(forbiddenWithUser): Accessors)(allowedWithoutUser =>
+            Restricted(
+              allowed = allowedWithoutUser,
+              forbidden = forbiddenWithUser
+            )
+          )
+    }
 
 }
