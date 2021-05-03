e9fb4cf322b9bdb553c82332d82195f7dc62faf1
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-05-04 00:28:48 +0200

    #7: Add order instance to userId

--- app/db/keys/UserId.scala
+++ app/db/keys/UserId.scala
@@ -1,8 +1,13 @@
 package db.keys
 
+import cats.Order
 import io.circe.generic.JsonCodec
 
 import java.util.UUID
 
 @JsonCodec
 case class UserId(uuid: UUID) extends AnyVal
+
+object UserId {
+  implicit val orderUserId: Order[UserId] = Order.by(_.uuid)
+}
