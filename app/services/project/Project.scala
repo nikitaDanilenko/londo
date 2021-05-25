09a96d0ec96b4e33e740543719b89d4cc05da0fb
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-05-25 23:29:16 +0200

    #7: Add project representation for frontend delivery

--- app/services/project/Project.scala
+++ app/services/project/Project.scala
@@ -15,3 +15,34 @@ case class Project(
     readAccessors: ProjectAccess[AccessKind.Read],
     writeAccessors: ProjectAccess[AccessKind.Write]
 )
+
+object Project {
+
+  case class Representation(
+      id: ProjectId,
+      plainTasks: Vector[Task.Plain],
+      projectReferenceTasks: Vector[Task.ProjectReference],
+      name: String,
+      description: Option[String],
+      ownerId: UserId,
+      parentProjectId: Option[ProjectId],
+      flatIfSingleTask: Boolean,
+      readAccessors: Accessors.Representation,
+      writeAccessors: Accessors.Representation
+  )
+
+  def toRepresentation(project: Project): Representation =
+    Representation(
+      id = project.id,
+      plainTasks = project.plainTasks,
+      projectReferenceTasks = project.projectReferenceTasks,
+      name = project.name,
+      description = project.description,
+      ownerId = project.ownerId,
+      parentProjectId = project.parentProjectId,
+      flatIfSingleTask = project.flatIfSingleTask,
+      readAccessors = Accessors.toRepresentation(project.readAccessors.accessors),
+      writeAccessors = Accessors.toRepresentation(project.writeAccessors.accessors)
+    )
+
+}
