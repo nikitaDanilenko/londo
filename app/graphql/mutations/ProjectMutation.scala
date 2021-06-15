ffab737ae7feb77e0ebdd450f0bb3d5db4b5c12d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-06-15 23:15:58 +0200

    #7: Wire access functions to project mutations

--- app/graphql/mutations/ProjectMutation.scala
+++ app/graphql/mutations/ProjectMutation.scala
@@ -1,9 +1,12 @@
 package graphql.mutations
 
 import graphql.HasGraphQLServices.syntax._
-import graphql.types.project.{ Project, ProjectCreation }
+import graphql.types.project.{ Accessors, Project, ProjectCreation, ProjectId }
 import graphql.{ HasGraphQLServices, HasLoggedInUser }
 import sangria.macros.derive.GraphQLField
+import services.project.{ AccessFromDB, AccessKind, ProjectAccess }
+import services.project.AccessFromDB.instances._
+import services.project.ProjectAccess.DbRepresentation
 
 import scala.concurrent.Future
 
@@ -19,5 +22,41 @@ trait ProjectMutation extends HasGraphQLServices with HasLoggedInUser {
       .unsafeToFuture()
       .handleServerError
       .map(Project.fromInternal)
+
+  @GraphQLField
+  def setReadAccess(
+      projectId: ProjectId,
+      accessors: Accessors
+  ): Future[Accessors] =
+    graphQLServices.projectService
+      .setReadAccess(
+        ProjectId.toInternal(projectId),
+        toServiceAccess[AccessKind.Read](accessors)
+      )
+      .unsafeToFuture()
+      .map(fromServiceAccess(_))
+
+  @GraphQLField
+  def setWriteAccess(
+      projectId: ProjectId,
+      accessors: Accessors
+  ): Future[Accessors] =
+    graphQLServices.projectService
+      .setWriteAccess(
+        ProjectId.toInternal(projectId),
+        toServiceAccess[AccessKind.Write](accessors)
+      )
+      .unsafeToFuture()
+      .map(fromServiceAccess(_))
+
+  private def toServiceAccess[AK](accessors: Accessors): ProjectAccess[AK] =
+    ProjectAccess[AK](services.project.Accessors.fromRepresentation(Accessors.toInternal(accessors)))
+
+  private def fromServiceAccess[AccessK, DBAccessK, DBAccessEntry](
+      dbRepresentation: DbRepresentation[DBAccessK, DBAccessEntry]
+  )(implicit accessFromDB: AccessFromDB[AccessK, DBAccessK, DBAccessEntry]): Accessors =
+    Accessors.fromInternal(
+      services.project.Accessors.toRepresentation(ProjectAccess.fromDb(dbRepresentation).accessors)
+    )
 
 }
