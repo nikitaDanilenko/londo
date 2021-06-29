681d18e505672f0e7ebaa57741e5d84304b2cdc2
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-06-30 00:24:45 +0200

    #7: Add utility functions for user validation

--- app/graphql/HasLoggedInUser.scala
+++ app/graphql/HasLoggedInUser.scala
@@ -1,19 +1,37 @@
 package graphql
 
 import cats.ApplicativeThrow
+import cats.data.NonEmptySet
+import cats.effect.MonadThrow
 import errors.{ ServerError, ServerException }
 import graphql.types.ToInternal.syntax._
 import graphql.types.user.UserId
+import services.project.Accessors
+import cats.syntax.flatMap._
+import cats.syntax.functor._
 
 trait HasLoggedInUser {
   protected def loggedInUserId: Option[UserId]
 
   final protected def validateAccess[F[_]: ApplicativeThrow](accessedUserId: UserId): F[services.user.UserId] =
+    allowedAccess(Accessors.NobodyExcept(NonEmptySet.of(accessedUserId.toInternal)))
+
+  final protected def allowedAccess[F[_]: ApplicativeThrow](accessors: Accessors): F[services.user.UserId] =
     ApplicativeThrow[F].fromEither(
       loggedInUserId
-        .filter(_ == accessedUserId)
+        .filter(userId => Accessors.hasAccess(userId.toInternal, accessors))
         .toRight(ServerException(ServerError.Authentication.Token.Restricted))
         .map(_.toInternal)
     )
+
+  final protected def allowedAccessVia[F[_]: MonadThrow, A, B](
+      fa: F[A]
+  )(accessorsOf: A => Accessors, conversion: A => B): F[B] =
+    fa.flatMap(a => allowedAccess(accessorsOf(a)).map(_ => conversion(a)))
+
+  final protected def allowedAccessViaError[F[_]: MonadThrow, A, B](
+      fa: F[ServerError.Valid[A]]
+  )(accessorsOf: A => Accessors, conversion: A => B): F[ServerError.Valid[B]] =
+    fa.flatMap(_.traverse(a => allowedAccess(accessorsOf(a)).map(_ => conversion(a))))
 
 }
