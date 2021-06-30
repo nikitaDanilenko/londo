12c531353a77ed36d78913d888e048c67615b585
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-06-30 23:49:29 +0200

    #7: Add task service to graphQL services

--- app/graphql/GraphQLServices.scala
+++ app/graphql/GraphQLServices.scala
@@ -1,6 +1,7 @@
 package graphql
 
 import services.project.ProjectService
+import services.task.TaskService
 import services.user.UserService
 
 import javax.inject.Inject
@@ -8,7 +9,8 @@ import scala.concurrent.ExecutionContext
 
 case class GraphQLServices @Inject() (
     userService: UserService,
-    projectService: ProjectService
+    projectService: ProjectService,
+    taskService: TaskService
 )(implicit
     val executionContext: ExecutionContext
 )
