96b0d7a9fff0c77acf3a255168810ecccc48c31e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-06 00:55:50 +0200

    #7: Propagate parameter removal

--- app/graphql/types/project/ProjectCreation.scala
+++ app/graphql/types/project/ProjectCreation.scala
@@ -2,19 +2,14 @@ package graphql.types.project
 
 import graphql.types.ToInternal
 import graphql.types.ToInternal.syntax._
-import graphql.types.user.UserId
+import io.circe.generic.JsonCodec
+import sangria.macros.derive.deriveInputObjectType
 import sangria.marshalling.FromInput
 import sangria.marshalling.circe.circeDecoderFromInput
-import sangria.macros.derive.deriveInputObjectType
 import sangria.schema.InputObjectType
-import spire.math.Natural
-import utils.json.CirceUtil.instances._
-import utils.graphql.SangriaUtil.instances._
-import io.circe.generic.JsonCodec
 
 @JsonCodec
 case class ProjectCreation(
-    ownerId: UserId,
     name: String,
     description: Option[String],
     parentProject: Option[ProjectId],
@@ -28,7 +23,6 @@ object ProjectCreation {
   implicit val projectCreationToInternal: ToInternal[ProjectCreation, services.project.ProjectCreation] =
     projectCreation =>
       services.project.ProjectCreation(
-        ownerId = projectCreation.ownerId.toInternal,
         name = projectCreation.name,
         description = projectCreation.description,
         parentProject = projectCreation.parentProject.map(_.toInternal),
