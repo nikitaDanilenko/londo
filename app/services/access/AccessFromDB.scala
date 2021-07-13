3c0df3e3347fc1336017646b53a023d9b60a518f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-13 23:36:04 +0200

    #9: Add instances, remove indirection of instance location

--- app/services/access/AccessFromDB.scala
+++ app/services/access/AccessFromDB.scala
@@ -1,6 +1,7 @@
 package services.access
 
-import db.models.{ ProjectReadAccess, ProjectReadAccessEntry, ProjectWriteAccess, ProjectWriteAccessEntry }
+import db.models._
+import services.dashboard.DashboardId
 import services.project.{ ProjectId, ReadAccessId, WriteAccessId }
 import services.user.UserId
 
@@ -24,39 +25,64 @@ sealed trait AccessFromDB[Id, AccessK, DBAccessK, DBAccessEntry] {
 
 object AccessFromDB {
 
-  trait Instances {
+  implicit val projectReadAccessFromDB
+      : AccessFromDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] =
+    new AccessFromDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] {
+      override type AccessId = ReadAccessId
 
-    implicit val projectReadAccessFromDB
-        : AccessFromDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] =
-      new AccessFromDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] {
-        override type AccessId = ReadAccessId
+      override def id(dbAccess: ProjectReadAccess): ProjectId = ProjectId(dbAccess.projectId)
+      override def accessId(dbAccess: ProjectReadAccess): AccessId = ReadAccessId(dbAccess.projectId)
 
-        override def id(dbAccess: ProjectReadAccess): ProjectId = ProjectId(dbAccess.projectId)
-        override def accessId(dbAccess: ProjectReadAccess): AccessId = ReadAccessId(dbAccess.projectId)
+      override def entryAccessId(dbAccessEntry: ProjectReadAccessEntry): AccessId =
+        ReadAccessId(dbAccessEntry.projectReadAccessId)
 
-        override def entryAccessId(dbAccessEntry: ProjectReadAccessEntry): AccessId =
-          ReadAccessId(dbAccessEntry.projectReadAccessId)
+      override def entryUserId(dbAccessEntry: ProjectReadAccessEntry): UserId = UserId(dbAccessEntry.userId)
+      override def isAllowList(dbAccess: ProjectReadAccess): Boolean = dbAccess.isAllowList
+    }
 
-        override def entryUserId(dbAccessEntry: ProjectReadAccessEntry): UserId = UserId(dbAccessEntry.userId)
-        override def isAllowList(dbAccess: ProjectReadAccess): Boolean = dbAccess.isAllowList
-      }
+  implicit val projectWriteAccessFromDB
+      : AccessFromDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] =
+    new AccessFromDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] {
 
-    implicit val projectWriteAccessFromDB
-        : AccessFromDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] =
-      new AccessFromDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] {
+      override type AccessId = WriteAccessId
+      override def id(dbAccess: ProjectWriteAccess): ProjectId = ProjectId(dbAccess.projectId)
+      override def accessId(dbAccess: ProjectWriteAccess): AccessId = WriteAccessId(dbAccess.projectId)
 
-        override type AccessId = WriteAccessId
-        override def id(dbAccess: ProjectWriteAccess): ProjectId = ProjectId(dbAccess.projectId)
-        override def accessId(dbAccess: ProjectWriteAccess): AccessId = WriteAccessId(dbAccess.projectId)
+      override def entryAccessId(dbAccessEntry: ProjectWriteAccessEntry): AccessId =
+        WriteAccessId(dbAccessEntry.projectWriteAccessId)
 
-        override def entryAccessId(dbAccessEntry: ProjectWriteAccessEntry): AccessId =
-          WriteAccessId(dbAccessEntry.projectWriteAccessId)
+      override def entryUserId(dbAccessEntry: ProjectWriteAccessEntry): UserId = UserId(dbAccessEntry.userId)
+      override def isAllowList(dbAccess: ProjectWriteAccess): Boolean = dbAccess.isAllowList
+    }
 
-        override def entryUserId(dbAccessEntry: ProjectWriteAccessEntry): UserId = UserId(dbAccessEntry.userId)
-        override def isAllowList(dbAccess: ProjectWriteAccess): Boolean = dbAccess.isAllowList
-      }
+  implicit val dashboardReadAccessFromDB
+      : AccessFromDB[DashboardId, AccessKind.Read, DashboardReadAccess, DashboardReadAccessEntry] =
+    new AccessFromDB[DashboardId, AccessKind.Read, DashboardReadAccess, DashboardReadAccessEntry] {
+      override type AccessId = ReadAccessId
 
-  }
+      override def id(dbAccess: DashboardReadAccess): DashboardId = DashboardId(dbAccess.dashboardId)
+      override def accessId(dbAccess: DashboardReadAccess): AccessId = ReadAccessId(dbAccess.dashboardId)
 
-  object instances extends Instances
+      override def entryAccessId(dbAccessEntry: DashboardReadAccessEntry): AccessId =
+        ReadAccessId(dbAccessEntry.dashboardReadAccessId)
+
+      override def entryUserId(dbAccessEntry: DashboardReadAccessEntry): UserId = UserId(dbAccessEntry.userId)
+      override def isAllowList(dbAccess: DashboardReadAccess): Boolean = dbAccess.isAllowList
+    }
+
+  implicit val dashboardWriteAccessFromDB
+      : AccessFromDB[DashboardId, AccessKind.Write, DashboardWriteAccess, DashboardWriteAccessEntry] =
+    new AccessFromDB[DashboardId, AccessKind.Write, DashboardWriteAccess, DashboardWriteAccessEntry] {
+
+      override type AccessId = WriteAccessId
+      override def id(dbAccess: DashboardWriteAccess): DashboardId = DashboardId(dbAccess.dashboardId)
+      override def accessId(dbAccess: DashboardWriteAccess): AccessId = WriteAccessId(dbAccess.dashboardId)
+
+      override def entryAccessId(dbAccessEntry: DashboardWriteAccessEntry): AccessId =
+        WriteAccessId(dbAccessEntry.dashboardWriteAccessId)
+
+      override def entryUserId(dbAccessEntry: DashboardWriteAccessEntry): UserId = UserId(dbAccessEntry.userId)
+      override def isAllowList(dbAccess: DashboardWriteAccess): Boolean = dbAccess.isAllowList
+    }
+
 }
--- app/services/access/AccessToDB.scala
+++ app/services/access/AccessToDB.scala
@@ -1,6 +1,7 @@
 package services.access
 
-import db.models.{ ProjectReadAccess, ProjectReadAccessEntry, ProjectWriteAccess, ProjectWriteAccessEntry }
+import db.models._
+import services.dashboard.DashboardId
 import services.project.ProjectId
 import services.user.UserId
 
@@ -13,45 +14,76 @@ sealed trait AccessToDB[Id, AccessK, DBAccessK, DBAccessEntry] {
 
 object AccessToDB {
 
-  trait Instances {
+  implicit val projectReadAccessToDB
+      : AccessToDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] =
+    new AccessToDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] {
 
-    implicit val projectReadAccessToDB
-        : AccessToDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] =
-      new AccessToDB[ProjectId, AccessKind.Read, ProjectReadAccess, ProjectReadAccessEntry] {
+      override def mkAccess(id: ProjectId, isAllowList: Boolean): ProjectReadAccess =
+        ProjectReadAccess(
+          projectId = id.uuid,
+          isAllowList = isAllowList
+        )
 
-        override def mkAccess(id: ProjectId, isAllowList: Boolean): ProjectReadAccess =
-          ProjectReadAccess(
-            projectId = id.uuid,
-            isAllowList = isAllowList
-          )
+      override def mkAccessEntry(id: ProjectId, userId: UserId): ProjectReadAccessEntry =
+        ProjectReadAccessEntry(
+          projectReadAccessId = id.uuid,
+          userId = userId.uuid
+        )
 
-        override def mkAccessEntry(id: ProjectId, userId: UserId): ProjectReadAccessEntry =
-          ProjectReadAccessEntry(
-            projectReadAccessId = id.uuid,
-            userId = userId.uuid
-          )
+    }
 
-      }
+  implicit val projectWriteAccessToDB
+      : AccessToDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] =
+    new AccessToDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] {
 
-    implicit val projectWriteAccessToDB
-        : AccessToDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] =
-      new AccessToDB[ProjectId, AccessKind.Write, ProjectWriteAccess, ProjectWriteAccessEntry] {
+      override def mkAccess(id: ProjectId, isAllowList: Boolean): ProjectWriteAccess =
+        ProjectWriteAccess(
+          projectId = id.uuid,
+          isAllowList = isAllowList
+        )
 
-        override def mkAccess(id: ProjectId, isAllowList: Boolean): ProjectWriteAccess =
-          ProjectWriteAccess(
-            projectId = id.uuid,
-            isAllowList = isAllowList
-          )
+      override def mkAccessEntry(id: ProjectId, userId: UserId): ProjectWriteAccessEntry =
+        ProjectWriteAccessEntry(
+          projectWriteAccessId = id.uuid,
+          userId = userId.uuid
+        )
 
-        override def mkAccessEntry(id: ProjectId, userId: UserId): ProjectWriteAccessEntry =
-          ProjectWriteAccessEntry(
-            projectWriteAccessId = id.uuid,
-            userId = userId.uuid
-          )
+    }
 
-      }
+  implicit val dashboardReadAccessToDB
+      : AccessToDB[DashboardId, AccessKind.Read, DashboardReadAccess, DashboardReadAccessEntry] =
+    new AccessToDB[DashboardId, AccessKind.Read, DashboardReadAccess, DashboardReadAccessEntry] {
 
-  }
+      override def mkAccess(id: DashboardId, isAllowList: Boolean): DashboardReadAccess =
+        DashboardReadAccess(
+          dashboardId = id.uuid,
+          isAllowList = isAllowList
+        )
 
-  object instances extends Instances
+      override def mkAccessEntry(id: DashboardId, userId: UserId): DashboardReadAccessEntry =
+        DashboardReadAccessEntry(
+          dashboardReadAccessId = id.uuid,
+          userId = userId.uuid
+        )
+
+    }
+
+  implicit val dashboardWriteAccessToDB
+      : AccessToDB[DashboardId, AccessKind.Write, DashboardWriteAccess, DashboardWriteAccessEntry] =
+    new AccessToDB[DashboardId, AccessKind.Write, DashboardWriteAccess, DashboardWriteAccessEntry] {
+
+      override def mkAccess(id: DashboardId, isAllowList: Boolean): DashboardWriteAccess =
+        DashboardWriteAccess(
+          dashboardId = id.uuid,
+          isAllowList = isAllowList
+        )
+
+      override def mkAccessEntry(id: DashboardId, userId: UserId): DashboardWriteAccessEntry =
+        DashboardWriteAccessEntry(
+          dashboardWriteAccessId = id.uuid,
+          userId = userId.uuid
+        )
+
+    }
+
 }
