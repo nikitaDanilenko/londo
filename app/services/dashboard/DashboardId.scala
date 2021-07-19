8f3d9508f3a1c60be345dc47a4a1ffe62ea3ef85
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-19 23:18:51 +0200

    #9: Add utility conversion functions as in the case of project ids

--- app/services/dashboard/DashboardId.scala
+++ app/services/dashboard/DashboardId.scala
@@ -1,5 +1,10 @@
 package services.dashboard
 
+import db.keys.{ DashboardReadAccessId, DashboardWriteAccessId }
+
 import java.util.UUID
 
-case class DashboardId(uuid: UUID)
+case class DashboardId(uuid: UUID) extends AnyVal {
+  def asDashboardReadAccessId: DashboardReadAccessId = DashboardReadAccessId(uuid)
+  def asDashboardWriteAccessId: DashboardWriteAccessId = DashboardWriteAccessId(uuid)
+}
