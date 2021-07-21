baa98808a6c2bb308e524ad41e0eafccb0bb421f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-21 23:33:39 +0200

    #9: Add GraphQL interface function for adding a dashboard

--- app/graphql/mutations/DashboardMutation.scala
+++ app/graphql/mutations/DashboardMutation.scala
@@ -1,12 +1,28 @@
 package graphql.mutations
 
+import graphql.HasGraphQLServices.syntax._
+import graphql.types.FromInternal.syntax._
+import graphql.types.ToInternal.syntax._
+import graphql.types.dashboard.{ Dashboard, DashboardCreation }
 import graphql.{ HasGraphQLServices, HasLoggedInUser }
+import sangria.macros.derive.GraphQLField
+
+import scala.concurrent.Future
 
 trait DashboardMutation extends HasGraphQLServices with HasLoggedInUser {
 
   import ioImplicits._
 
-  def createDashboard = ???
+  @GraphQLField
+  def createDashboard(dashboardCreation: DashboardCreation): Future[Dashboard] = {
+    withUser { userId =>
+      graphQLServices.dashboardService
+        .create(userId.toInternal, dashboardCreation.toInternal)
+    }
+      .map(_.fromInternal[Dashboard])
+      .unsafeToFuture()
+      .handleServerError
+  }
 
   def updateDashboard = ???
 
