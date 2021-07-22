9aed9c83073e0859ec70e516e73da02605bb3762
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-23 00:17:22 +0200

    #9: Add GraphQL representation for weights on dashboards

---
+++ app/graphql/types/dashboard/ProjectWeightOnDashboard.scala
@@ -0,0 +1,49 @@
+package graphql.types.dashboard
+
+import graphql.types.FromAndToInternal
+import graphql.types.FromInternal.syntax._
+import graphql.types.ToInternal.syntax._
+import graphql.types.project.ProjectId
+import io.circe.generic.JsonCodec
+import sangria.macros.derive.{ InputObjectTypeName, deriveInputObjectType, deriveObjectType }
+import sangria.marshalling.FromInput
+import sangria.marshalling.circe.circeDecoderFromInput
+import sangria.schema.{ InputObjectType, ObjectType }
+import spire.math.Natural
+import utils.json.CirceUtil.instances._
+
+@JsonCodec
+case class ProjectWeightOnDashboard(
+    projectId: ProjectId,
+    weight: Natural
+)
+
+object ProjectWeightOnDashboard {
+
+  implicit val projectWeightOnDashboardFromAndToInternal
+      : FromAndToInternal[ProjectWeightOnDashboard, services.dashboard.ProjectWeightOnDashboard] =
+    FromAndToInternal.create(
+      internal =>
+        ProjectWeightOnDashboard(
+          projectId = internal.projectId.fromInternal,
+          weight = internal.weight
+        ),
+      projectWeightOnDashboard =>
+        services.dashboard.ProjectWeightOnDashboard(
+          projectId = projectWeightOnDashboard.projectId.toInternal,
+          weight = projectWeightOnDashboard.weight
+        )
+    )
+
+  implicit val projectWeightOnDashboardObjectType: ObjectType[Unit, ProjectWeightOnDashboard] =
+    deriveObjectType[Unit, ProjectWeightOnDashboard]()
+
+  implicit val projectWeightOnDashboardInputObjectType: InputObjectType[ProjectWeightOnDashboard] =
+    deriveInputObjectType[ProjectWeightOnDashboard](
+      InputObjectTypeName("ProjectWeightOnDashboardInput")
+    )
+
+  implicit lazy val projectWeightOnDashboardFromInput: FromInput[ProjectWeightOnDashboard] =
+    circeDecoderFromInput[ProjectWeightOnDashboard]
+
+}
