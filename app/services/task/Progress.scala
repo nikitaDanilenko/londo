35f19802053052109c8600ac52e59278d0cbb9e3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-27 23:55:25 +0200

    #9: Add utility functions and instances to progress

--- app/services/task/Progress.scala
+++ app/services/task/Progress.scala
@@ -1,19 +1,22 @@
 package services.task
 
-import algebra.ring.AdditiveMonoid
-import spire.algebra.Order
+import algebra.ring.{ AdditiveMonoid, AdditiveSemigroup }
+import math.Positive
+import spire.algebra.{ MultiplicativeSemigroup, Order }
 import spire.math.{ Natural, Rational }
+import spire.syntax.multiplicativeMonoid._
+import utils.math.NaturalUtil
 
 sealed trait Progress {
   def reached: Natural
-  def reachable: Natural
+  def reachable: Positive
 
   def set(reachedValue: Natural): Progress
 
   final def value: Rational =
     Rational(
       n = reached.toBigInt,
-      d = reachable.toBigInt
+      d = reachable.natural.toBigInt
     )
 
 }
@@ -22,29 +25,51 @@ object Progress {
 
   private case class Impl(
       override val reached: Natural,
-      override val reachable: Natural
+      override val reachable: Positive
   ) extends Progress {
 
     override def set(reachedValue: Natural): Progress =
       copy(
         reached = clampMax(
           value = reachedValue,
-          max = reachable
+          max = reachable.natural
         )
       )
 
   }
 
-  def zero(reachable: Natural): Progress =
+  def zero(reachable: Positive): Progress =
     Impl(
       reached = Natural.zero,
       reachable = reachable
     )
 
-  def fraction(reachable: Natural, reached: Natural): Progress =
+  def fraction(reachable: Positive, reached: Natural): Progress =
     zero(reachable).set(reached)
+
+  def divideBy(progress: Progress, p: Positive): Progress =
+    unsafeFromRational(progress.value * Rational(1, p.natural.toBigInt))
 
   private def clampMax[A: Order: AdditiveMonoid](value: A, max: A): A =
     Order[A].min(max, value)
+
+  private def unsafeFromRational(rational: Rational): Progress =
+    Progress.fraction(
+      // The conversion should be safe, since denominators are always positive
+      reachable = NaturalUtil
+        .fromBigInt(rational.denominator.toBigInt)
+        .flatMap(Positive(_))
+        .toOption
+        .get,
+      // This conversion is only safe for non-negative natural numbers
+      reached = Natural(rational.numerator.toBigInt)
+    )
+
+  implicit val progressAdditiveSemigroup: AdditiveSemigroup[Progress] = { (x, y) =>
+    unsafeFromRational(x.value + y.value)
+  }
+
+  implicit val progressMultiplicativeSemigroup: MultiplicativeSemigroup[Progress] = (x, y) =>
+    fraction(x.reachable * y.reachable, x.reached * y.reached)
 
 }
