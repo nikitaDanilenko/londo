bb9e1308496723e3fb6fe80a7dfc38f365abbedc
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-31 23:57:22 +0200

    #9: Add and use utility functions for non-empty lists

--- app/services/task/WeightedProgress.scala
+++ app/services/task/WeightedProgress.scala
@@ -4,6 +4,7 @@ import cats.data.NonEmptyList
 import math.Positive
 import spire.syntax.additiveSemigroup._
 import spire.syntax.multiplicativeSemigroup._
+import utils.fp.NonEmptyListUtil
 
 case class WeightedProgress(
     weight: Positive,
@@ -15,10 +16,7 @@ object WeightedProgress {
   def rescaledProgress(overallWeight: Positive)(weightedProgress: WeightedProgress): Progress =
     Progress.fraction(overallWeight, weightedProgress.weight.natural) * weightedProgress.progress
 
-  def average(progresses: NonEmptyList[WeightedProgress], overallWeight: Positive): Progress = {
-    progresses.tail.foldLeft(rescaledProgress(overallWeight)(progresses.head))((r, wp) =>
-      r + rescaledProgress(overallWeight)(wp)
-    )
-  }
+  def average(progresses: NonEmptyList[WeightedProgress], overallWeight: Positive): Progress =
+    NonEmptyListUtil.foldMapLeft(progresses, rescaledProgress(overallWeight))(_ + _)
 
 }
