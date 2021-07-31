b109c96ac03e7f8cca62eaefb75f4614d39db425
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-31 23:36:42 +0200

    #9: Delegate average computation to caller, since there may be more weights than progresses

--- app/services/task/WeightedProgress.scala
+++ app/services/task/WeightedProgress.scala
@@ -15,14 +15,10 @@ object WeightedProgress {
   def rescaledProgress(overallWeight: Positive)(weightedProgress: WeightedProgress): Progress =
     Progress.fraction(overallWeight, weightedProgress.weight.natural) * weightedProgress.progress
 
-  def average(progresses: NonEmptyList[WeightedProgress]): Progress = {
-    val overallWeight = progresses.tail
-      .foldLeft(progresses.head.weight)((w, wp) => w + wp.weight)
-
+  def average(progresses: NonEmptyList[WeightedProgress], overallWeight: Positive): Progress = {
     progresses.tail.foldLeft(rescaledProgress(overallWeight)(progresses.head))((r, wp) =>
       r + rescaledProgress(overallWeight)(wp)
     )
-
   }
 
 }
