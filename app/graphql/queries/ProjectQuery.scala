87ff0f12b7525008db354cfbfabfdb7cc1b26148
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-07-31 22:54:33 +0200

    #9: Add function for fetching the project progress of an individual project

--- app/graphql/queries/ProjectQuery.scala
+++ app/graphql/queries/ProjectQuery.scala
@@ -1,11 +1,14 @@
 package graphql.queries
 
+import cats.data.EitherT
 import cats.effect.IO
 import errors.ServerError
 import graphql.types.FromInternal.syntax._
 import graphql.types.project.{ Project, ProjectId }
+import graphql.types.task.Progress
 import graphql.{ HasGraphQLServices, HasLoggedInUser }
 import sangria.macros.derive.GraphQLField
+import services.project.ResolvedProject
 
 import scala.concurrent.Future
 
@@ -16,6 +19,14 @@ trait ProjectQuery extends HasGraphQLServices with HasLoggedInUser {
   def fetchProject(projectId: ProjectId): Future[Project] = {
     validateProjectReadAccess(projectId)((_, project) => IO.pure(ServerError.result(project.fromInternal[Project])))
   }
+
+  @GraphQLField
+  def projectProgress(projectId: ProjectId): Future[Option[Progress]] =
+    validateProjectReadAccess(projectId) { (_, project) =>
+      EitherT(graphQLServices.projectService.resolveProject(project.id))
+        .map(ResolvedProject.progress(_).map(_.fromInternal[Progress]))
+        .value
+    }
 
   private def validateProjectReadAccess[A](
       projectId: ProjectId
