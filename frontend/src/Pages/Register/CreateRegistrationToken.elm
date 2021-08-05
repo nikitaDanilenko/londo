4cee19e4468fb927938f15e41f31720481d1fcca
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-08-05 23:37:36 +0200

    #12: Set up preliminary GraphQL mutation call

--- frontend/src/Pages/Register/CreateRegistrationToken.elm
+++ frontend/src/Pages/Register/CreateRegistrationToken.elm
@@ -1,15 +1,21 @@
 module Pages.Register.CreateRegistrationToken exposing (..)
 
 import Basics.Extra exposing (flip)
+import Graphql.Http
+import Graphql.Operation exposing (RootMutation, RootQuery)
+import Graphql.SelectionSet exposing (SelectionSet)
 import Html exposing (Html, button, div, input, text)
 import Html.Attributes exposing (class, id)
 import Html.Events exposing (onClick, onInput)
 import Language.Language exposing (Language)
-
+import LondoGQL.Mutation as Mutation
+import LondoGQL.Scalar exposing (Unit)
+import RemoteData exposing (RemoteData)
 
 type Msg
     = RequestToken
     | ChangeEmail String
+    | GotResponse (RemoteData (Graphql.Http.Error Unit) Unit)
 
 
 type alias Model =
@@ -32,10 +38,13 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         RequestToken ->
-            ( model, Cmd.none )
+            ( model, makeRequest model )
 
         ChangeEmail string ->
             ( model |> flip updateEmail string, Cmd.none )
+
+        GotResponse remoteData ->
+            (model, Cmd.none)
 
 
 view : Model -> Html Msg
@@ -45,4 +54,15 @@ view model =
         , div [ id "registrationButton" ] [ button [ class "requestButton", onClick RequestToken ] [ text model.language.requestTokenForRegistration ] ]
         ]
 
---requestToken : String -> Cmd Msg
+
+requestTokenQuery : Model -> SelectionSet Unit RootMutation
+requestTokenQuery model =
+    Mutation.requestCreate { email = model.email }
+
+
+makeRequest : Model -> Cmd Msg
+makeRequest model =
+    requestTokenQuery model
+        |> Graphql.Http.mutationRequest "http://localhost:9000/graphql"
+        |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
+
