9a36b9bc9396742c6f8b2aa9a1eb041ab639ebb3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-08-16 22:17:25 +0200

    #12: Incorporate placeholders for email and token

--- app/services/user/FrontendConfiguration.scala
+++ app/services/user/FrontendConfiguration.scala
@@ -4,6 +4,8 @@ import play.api.Configuration
 
 sealed abstract case class FrontendConfiguration(
     url: String,
+    emailPlaceholder: String,
+    tokenPlaceholder: String,
     registrationPattern: String
 )
 
@@ -12,6 +14,8 @@ object FrontendConfiguration {
   def apply(configuration: Configuration): FrontendConfiguration =
     new FrontendConfiguration(
       url = configuration.get[String]("application.frontend.url"),
+      emailPlaceholder = configuration.get[String]("application.frontend.emailPlaceholder"),
+      tokenPlaceholder = configuration.get[String]("application.frontend.tokenPlaceholder"),
       registrationPattern = configuration.get[String]("application.frontend.registrationPattern")
     ) {}
 
