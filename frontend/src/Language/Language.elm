abaf5a3564d55c44a89035e101da0f3eccf4ffc5
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-08-16 23:05:34 +0200

    #12: Extend language components

--- frontend/src/Language/Language.elm
+++ frontend/src/Language/Language.elm
@@ -4,6 +4,8 @@ module Language.Language exposing (..)
 type alias Language =
     { enterEmailForRegistrationRequest : String
     , requestTokenForRegistration : String
+    , tokenRequestSuccessful: String
+    , tokenRequestFailed: String
     }
 
 
@@ -16,6 +18,8 @@ english : Language
 english =
     { enterEmailForRegistrationRequest = "Email for registration"
     , requestTokenForRegistration = "Request registration"
+    , tokenRequestSuccessful = "Successfully requested token! Check your email to proceed!"
+    , tokenRequestFailed = "There was an error requesting the token. Please try again!"
     }
 
 
