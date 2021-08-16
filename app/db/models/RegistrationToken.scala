7ad77aff008743cd2a78302c5b1e01bf91b617f7
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-08-16 22:17:47 +0200

    #12: Add function for creating a registration link to the frontend

--- app/db/models/RegistrationToken.scala
+++ app/db/models/RegistrationToken.scala
@@ -1,3 +1,21 @@
 package db.models
 
+import services.user.FrontendConfiguration
+
 case class RegistrationToken(email: String, token: String)
+
+object RegistrationToken {
+
+  def createRegistrationLink(
+      frontendConfiguration: FrontendConfiguration,
+      registrationToken: RegistrationToken
+  ): String = {
+    pprint.log(frontendConfiguration)
+    val replaced = frontendConfiguration.registrationPattern
+      .replace(frontendConfiguration.emailPlaceholder, registrationToken.email)
+      .replace(frontendConfiguration.tokenPlaceholder, registrationToken.token)
+
+    s"${frontendConfiguration.url}$replaced"
+  }
+
+}
