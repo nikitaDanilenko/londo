1bf7225ef6013fe381f56d21a9a8fec306ac4e9b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-08-16 23:54:11 +0200

    #12: Incorporate user creation page into main page

--- frontend/src/Pages/MainPage.elm
+++ frontend/src/Pages/MainPage.elm
@@ -5,6 +5,7 @@ import Browser exposing (UrlRequest)
 import Browser.Navigation as Nav
 import Html exposing (Html, div, text)
 import Language.Language as Language exposing (Language)
+import Pages.Register.CreateNewUser as CreateNewUser
 import Pages.Register.CreateRegistrationToken as CreateRegistrationToken
 import Url exposing (Protocol(..), Url)
 import Url.Parser as Parser exposing ((</>), Parser, s)
@@ -30,6 +31,7 @@ type alias Model =
 
 type Page
     = CreateRegistrationToken CreateRegistrationToken.Model
+    | CreateNewUser CreateNewUser.Model
     | NotFound
 
 
@@ -37,6 +39,7 @@ type Msg
     = ClickedLink UrlRequest
     | ChangedUrl Url
     | CreateRegistrationTokenMsg CreateRegistrationToken.Msg
+    | CreateNewUserMsg CreateNewUser.Msg
 
 
 titleFor : Model -> String
@@ -54,6 +57,9 @@ view model =
     case model.page of
         CreateRegistrationToken createRegistrationToken ->
             Html.map CreateRegistrationTokenMsg (CreateRegistrationToken.view createRegistrationToken)
+
+        CreateNewUser createNewUser ->
+            Html.map CreateNewUserMsg (CreateNewUser.view createNewUser)
 
         NotFound ->
             div [] [ text "Page not found" ]
@@ -81,6 +87,14 @@ update msg model =
                 _ ->
                     ( model, Cmd.none )
 
+        CreateNewUserMsg createNewUserMsg ->
+            case model.page of
+                CreateNewUser createNewUser ->
+                    stepCreateNewUser model (CreateNewUser.update createNewUserMsg createNewUser)
+
+                _ ->
+                    ( model, Cmd.none )
+
 
 stepTo : Url -> Model -> ( Model, Cmd Msg )
 stepTo url model =
@@ -89,6 +103,9 @@ stepTo url model =
             case answer of
                 CreateRegistrationTokenRoute language ->
                     CreateRegistrationToken.init language |> stepCreateRegistrationToken model
+
+                CreateNewUserRoute flags ->
+                    CreateNewUser.init flags |> stepCreateNewUser model
 
         Nothing ->
             ( { model | page = NotFound }, Cmd.none )
@@ -99,8 +116,14 @@ stepCreateRegistrationToken model ( createRegistrationToken, cmd ) =
     ( { model | page = CreateRegistrationToken createRegistrationToken }, Cmd.map CreateRegistrationTokenMsg cmd )
 
 
+stepCreateNewUser : Model -> ( CreateNewUser.Model, Cmd CreateNewUser.Msg ) -> ( Model, Cmd Msg )
+stepCreateNewUser model ( createNewUser, cmd ) =
+    ( { model | page = CreateNewUser createNewUser }, Cmd.map CreateNewUserMsg cmd )
+
+
 type Route
     = CreateRegistrationTokenRoute Language
+    | CreateNewUserRoute CreateNewUser.Flags
 
 
 routeParser : Parser (Route -> a) a
