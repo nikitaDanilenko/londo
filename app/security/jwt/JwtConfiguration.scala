94d11100baa4eedf0b61ebd7e46fe5bf80279f72
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-08-16 21:53:18 +0200

    #12: Rearrange configuration into abstract case class

--- app/security/jwt/JwtConfiguration.scala
+++ app/security/jwt/JwtConfiguration.scala
@@ -2,27 +2,19 @@ package security.jwt
 
 import play.api.Configuration
 
-sealed trait JwtConfiguration {
-
-  def signaturePublicKey: String
-  def signaturePrivateKey: String
-
-  def restrictedDurationInSeconds: Long
-}
+sealed abstract case class JwtConfiguration(
+    signaturePublicKey: String,
+    signaturePrivateKey: String,
+    restrictedDurationInSeconds: Long
+)
 
 object JwtConfiguration {
 
-  private case class JwtConfigurationImpl(
-      override val signaturePublicKey: String,
-      override val signaturePrivateKey: String,
-      override val restrictedDurationInSeconds: Long
-  ) extends JwtConfiguration
-
   def apply(configuration: Configuration): JwtConfiguration =
-    JwtConfigurationImpl(
+    new JwtConfiguration(
       signaturePublicKey = configuration.get[String]("application.jwt.signaturePublicKey"),
       signaturePrivateKey = configuration.get[String]("application.jwt.signaturePrivateKey"),
       restrictedDurationInSeconds = configuration.get[Long]("application.jwt.restrictedDurationInSeconds")
-    )
+    ) {}
 
 }
