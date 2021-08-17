5e62383bce6b0d58a45b43f1d3c1ed5973e0f6fb
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-08-17 23:46:16 +0200

    #12: Add sub-folders for login and registration

--- frontend/src/Configuration.elm
+++ frontend/src/Configuration.elm
@@ -1,7 +1,16 @@
 module Configuration exposing (..)
 
 --todo: Keep watch for possible restructuring; this may be sensible to provide only the relevant configuration flags
+
+
 type alias Configuration =
     { graphQLEndpoint : String
     , mainPageURL : String
+    , subFolders : SubFolders
+    }
+
+
+type alias SubFolders =
+    { register : String
+    , login : String
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,10 @@ Elm.Main.init({
   node: document.getElementById('root'),
   flags: {
     graphQLEndpoint: process.env.ELM_APP_LONDO_GRAPH_QL_ENDPOINT,
-    mainPageURL: process.env.ELM_APP_LONDO_MAIN_PAGE_URL
+    mainPageURL: process.env.ELM_APP_LONDO_MAIN_PAGE_URL,
+    subFolders : {
+      login: process.env.ELM_APP_LONDO_SUBFOLDER_LOGIN,
+      register: process.env.ELM_APP_LONDO_SUBFOLDER_REGISTER
+    }
   }
 });
