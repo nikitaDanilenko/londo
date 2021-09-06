daac32d785e72f8ed56a4609182895abb8e38a46
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-09-06 22:57:06 +0200

    #12: Extend signature configuration with backend exponent

--- app/security/SignatureConfiguration.scala
+++ app/security/SignatureConfiguration.scala
@@ -4,7 +4,8 @@ import play.api.Configuration
 
 sealed abstract case class SignatureConfiguration(
     modulus: BigInt,
-    primitiveRoot: BigInt
+    primitiveRoot: BigInt,
+    backendExponent: BigInt
 )
 
 object SignatureConfiguration {
@@ -12,7 +13,8 @@ object SignatureConfiguration {
   def apply(configuration: Configuration): SignatureConfiguration =
     new SignatureConfiguration(
       modulus = BigInt(configuration.get[String]("application.signature.modulus")),
-      primitiveRoot = BigInt(configuration.get[String]("application.signature.primitiveRoot"))
+      primitiveRoot = BigInt(configuration.get[String]("application.signature.primitiveRoot")),
+      backendExponent = BigInt(configuration.get[String]("application.signature.backendExponent"))
     ) {}
 
 }
