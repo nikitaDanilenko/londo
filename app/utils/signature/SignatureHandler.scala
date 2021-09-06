21c9a35f1229d5cd39cdf1f37b838cbb0b4d2349
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-09-06 23:11:11 +0200

    #12: Adjust signature handler to HMAC logic

--- app/utils/signature/SignatureHandler.scala
+++ app/utils/signature/SignatureHandler.scala
@@ -2,25 +2,21 @@ package utils.signature
 
 import utils.string.StringUtil.syntax._
 
-import java.security.Signature
-import scala.util.Try
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 object SignatureHandler {
 
-  val signatureAlgorithm: String = "SHA512withRSA"
+  val signatureAlgorithm: String = "HmacSHA512"
 
-  def validate(signature: String, message: String, publicKey: String): Boolean = {
-    val sig = Signature.getInstance(signatureAlgorithm)
-    sig.initVerify(RSAUtil.publicKeyFromBase64String(publicKey))
-    sig.update(message.getBytes)
-    Try(sig.verify(signature.asBase64ByteArray)).getOrElse(false)
-  }
+  def validate(signature: String, message: String, secret: String): Boolean =
+    signature == sign(message, secret)
 
-  def sign(message: String, privateKey: String): String = {
-    val signature = Signature.getInstance(signatureAlgorithm)
-    signature.initSign(RSAUtil.privateKeyFromBase64String(privateKey))
-    signature.update(message.getBytes)
-    signature.sign().asBase64String
+  def sign(message: String, secret: String): String = {
+    val mac = Mac.getInstance(signatureAlgorithm)
+    mac.init(new SecretKeySpec(secret.getBytes, signatureAlgorithm))
+    mac.update(secret.getBytes)
+    mac.doFinal(message.getBytes).asBase64String
   }
 
 }
