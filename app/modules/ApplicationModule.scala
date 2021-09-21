c69710eccb28015093b5d4eb46c46fb3016eab45
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-09-21 22:00:43 +0200

    Revert "#12: Remove signature configuration"
    This reverts commit 3733b006

--- app/modules/ApplicationModule.scala
+++ app/modules/ApplicationModule.scala
@@ -3,6 +3,7 @@ package modules
 import db.DbConnection
 import play.api.inject.Binding
 import play.api.{ Configuration, Environment }
+import security.SignatureConfiguration
 import security.jwt.JwtConfiguration
 import services.user.FrontendConfiguration
 
@@ -15,6 +16,7 @@ class ApplicationModule extends play.api.inject.Module {
 
     val configurations = Seq(
       bind[JwtConfiguration].toInstance(JwtConfiguration(configuration)),
+      bind[SignatureConfiguration].toInstance(SignatureConfiguration(configuration)),
       bind[FrontendConfiguration].toInstance(FrontendConfiguration(configuration))
     )
 
