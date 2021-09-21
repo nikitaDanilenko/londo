13fed137ec83ce1cda97c7052ef96ece8ea261a2
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-09-21 23:25:24 +0200

    #12: Adjust keys, and key parsing from strings

--- app/utils/signature/SignatureHandler.scala
+++ app/utils/signature/SignatureHandler.scala
@@ -2,13 +2,13 @@ package utils.signature
 
 import utils.string.StringUtil.syntax._
 
-import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.{ PKCS8EncodedKeySpec, X509EncodedKeySpec }
 import java.security.{ KeyFactory, PrivateKey, PublicKey, Signature }
 
 object SignatureHandler {
 
-  val signatureAlgorithm: String = "SHA3-512withRSA"
-  val keyAlgorithm: String = "RSA"
+  val signatureAlgorithm: String = "SHA512withECDSA"
+  val keyAlgorithm: String = "EC"
 
   def sign(message: String, privateKey: PrivateKey): String = {
     val signature = Signature.getInstance(signatureAlgorithm)
@@ -17,14 +17,21 @@ object SignatureHandler {
     signature.sign().asBase64String
   }
 
+  def validate(message: String, publicKey: PublicKey, signatureString: String): Boolean = {
+    val signature = Signature.getInstance(signatureAlgorithm)
+    signature.initVerify(publicKey)
+    signature.update(message.getBytes)
+    signature.verify(signatureString.asBase64ByteArray)
+  }
+
   def privateKeyFromPKCS8String(string: String): PrivateKey = {
     val keyFactory = KeyFactory.getInstance(keyAlgorithm)
     keyFactory.generatePrivate(new PKCS8EncodedKeySpec(string.asBase64ByteArray))
   }
 
-  def publicKeyFromPKCS8String(string: String): PublicKey = {
+  def publicKeyFromX509String(string: String): PublicKey = {
     val keyFactory = KeyFactory.getInstance(keyAlgorithm)
-    keyFactory.generatePublic(new PKCS8EncodedKeySpec(string.asBase64ByteArray))
+    keyFactory.generatePublic(new X509EncodedKeySpec(string.asBase64ByteArray))
   }
 
 }
