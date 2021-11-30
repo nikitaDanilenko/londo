5908c255e0dea380047b4df1b92e9a602397e5bd
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-11-30 23:21:36 +0100

    #15: Add min function on natural numbers

--- frontend/src/Pages/Util/ScalarUtil.elm
+++ frontend/src/Pages/Util/ScalarUtil.elm
@@ -1,14 +1,34 @@
 module Pages.Util.ScalarUtil exposing (..)
 
-import LondoGQL.Scalar exposing (Natural (..), Positive (..), Uuid (..))
+import Integer
+import LondoGQL.Scalar exposing (Natural(..), Positive(..), Uuid(..))
 
 
 uuidToString : Uuid -> String
 uuidToString (Uuid uuid) =
     uuid
 
+
 positiveToString : Positive -> String
-positiveToString (Positive positive) = positive
+positiveToString (Positive positive) =
+    positive
+
 
 naturalToString : Natural -> String
-naturalToString (Natural natural) = natural
+naturalToString (Natural natural) =
+    natural
+
+
+minNatural : Natural -> Natural -> Natural
+minNatural a b =
+    let
+        asInteger =
+            naturalToString
+                >> Integer.fromString
+                >> Maybe.withDefault Integer.zero
+    in
+    if Integer.lte (asInteger a) (asInteger b) then
+        a
+
+    else
+        b
