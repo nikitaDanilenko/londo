04cd30b83323a52e0db01f4e6bb5db1baec64365
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-12-01 00:02:11 +0100

    #15: Add parsing and conversion functions for positive numbers

--- frontend/src/Pages/Util/ScalarUtil.elm
+++ frontend/src/Pages/Util/ScalarUtil.elm
@@ -2,6 +2,7 @@ module Pages.Util.ScalarUtil exposing (..)
 
 import Integer
 import LondoGQL.Scalar exposing (Natural(..), Positive(..), Uuid(..))
+import Maybe.Extra
 
 
 uuidToString : Uuid -> String
@@ -14,9 +15,33 @@ positiveToString (Positive positive) =
     positive
 
 
+positiveToNatural : Positive -> Natural
+positiveToNatural (Positive p) =
+    Natural p
+
+
 naturalToString : Natural -> String
 naturalToString (Natural natural) =
     natural
+
+
+stringToNatural : String -> Maybe Natural
+stringToNatural s =
+    Integer.fromString s
+        |> Maybe.Extra.filter (Integer.lte Integer.zero)
+        |> Maybe.map (always (Natural s))
+
+
+stringToPositive : String -> Maybe Positive
+stringToPositive s =
+    Integer.fromString s
+        |> Maybe.Extra.filter (Integer.lt Integer.zero)
+        |> Maybe.map (always (Positive s))
+
+
+zeroNatural : Natural
+zeroNatural =
+    Natural "0"
 
 
 minNatural : Natural -> Natural -> Natural
