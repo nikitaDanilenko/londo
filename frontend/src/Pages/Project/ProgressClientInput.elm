5474227eb382039cca5f657720dee5f928e51efb
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-12-01 00:09:33 +0100

    #15: Add conversion functions and lenses

--- frontend/src/Pages/Project/ProgressClientInput.elm
+++ frontend/src/Pages/Project/ProgressClientInput.elm
@@ -2,7 +2,9 @@ module Pages.Project.ProgressClientInput exposing (..)
 
 import LondoGQL.InputObject exposing (ProgressInput)
 import LondoGQL.Scalar exposing (Natural, Positive)
+import Monocle.Lens exposing (Lens)
 import Pages.Util.FromInput as FromInput exposing (FromInput)
+import Pages.Util.ScalarUtil as ScalarUtil
 
 
 type alias ProgressClientInput =
@@ -18,8 +20,31 @@ default =
     }
 
 
+from : FromInput ProgressInput -> ProgressClientInput
+from fi =
+    { reachable =
+        FromInput.positive
+            |> FromInput.value.set fi.value.reachable
+            |> FromInput.text.set (ScalarUtil.positiveToString fi.value.reachable)
+    , reached =
+        FromInput.natural
+            |> FromInput.value.set fi.value.reached
+            |> FromInput.text.set (ScalarUtil.naturalToString fi.value.reached)
+    }
+
+
 to : ProgressClientInput -> ProgressInput
 to input =
     { reachable = input.reachable.value
     , reached = input.reached.value
     }
+
+
+reachable : Lens ProgressClientInput (FromInput Positive)
+reachable =
+    Lens .reachable (\b a -> { a | reachable = b })
+
+
+reached : Lens ProgressClientInput (FromInput Natural)
+reached =
+    Lens .reached (\b a -> { a | reached = b })
