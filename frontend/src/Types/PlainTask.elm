9cd7596d9af386984ff4c3e23b6b915fd6e029d4
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-02-08 23:06:44 +0100

    #15: Add lenses for all relevant fields of PlainTask

--- frontend/src/Types/PlainTask.elm
+++ frontend/src/Types/PlainTask.elm
@@ -1,9 +1,10 @@
 module Types.PlainTask exposing (..)
 
-import Graphql.OptionalArgument as OptionalArgumentUtil
+import Graphql.OptionalArgument as OptionalArgument
 import LondoGQL.Enum.TaskKind exposing (TaskKind)
 import LondoGQL.InputObject exposing (PlainUpdate)
 import LondoGQL.Scalar exposing (Positive)
+import Monocle.Lens exposing (Lens)
 import Types.Progress exposing (Progress)
 import Types.TaskId exposing (TaskId)
 
@@ -22,6 +23,30 @@ toUpdate : PlainTask -> PlainUpdate
 toUpdate plainTask =
     { name = plainTask.name
     , taskKind = plainTask.taskKind
-    , unit = OptionalArgumentUtil.fromMaybe plainTask.unit
+    , unit = OptionalArgument.fromMaybe plainTask.unit
     , weight = plainTask.weight
     }
+
+name : Lens PlainTask String
+name =
+    Lens .name (\b a -> { a | name = b })
+
+
+taskKind : Lens PlainTask TaskKind
+taskKind =
+    Lens .taskKind (\b a -> { a | taskKind = b })
+
+
+unit : Lens PlainTask (Maybe String)
+unit =
+    Lens .unit (\b a -> { a | unit = b })
+
+
+progress : Lens PlainTask Progress
+progress =
+    Lens .progress (\b a -> { a | progress = b })
+
+
+weight : Lens PlainTask Positive
+weight =
+    Lens .weight (\b a -> { a | weight = b })
