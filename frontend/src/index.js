02de2c296c936745bd83305b5dd08dba6ba8ad37
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-05-11 23:49:00 +0200

    #15: Incorporate the environment variable for the name of the task editor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,8 @@ Elm.Main.init({
       login: process.env.ELM_APP_LONDO_SUBFOLDER_LOGIN,
       register: process.env.ELM_APP_LONDO_SUBFOLDER_REGISTER,
       overview: process.env.ELM_APP_LONDO_SUBFOLDER_OVERVIEW,
-      newProject: process.env.ELM_APP_LONDO_SUBFOLDER_NEW_PROJECT
+      newProject: process.env.ELM_APP_LONDO_SUBFOLDER_NEW_PROJECT,
+      taskEditor: process.env.ELM_APP_LONDO_SUBFOLDER_TASK_EDITOR
     }
   }
 });
