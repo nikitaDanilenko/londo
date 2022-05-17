ea15c016bc6f278073fb81482705ec8f19a3183d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-05-17 21:44:48 +0200

    #15: Remove unused implicit

--- app/graphql/types/util/Natural.scala
+++ app/graphql/types/util/Natural.scala
@@ -16,13 +16,10 @@ object Natural {
 
   private def apply(nonNegative: Int): Natural = new Natural(nonNegative) {}
 
-  implicit val naturalCodec: Codec[Natural] = deriveCodec[Natural].iemap(
-    Right(_).filterOrElse(_.nonNegative >= 0, "Non a non-negative number")
-  )(identity)
-
-  implicit val naturalDecoder: Decoder[Natural] = deriveDecoder[Natural].emap { n =>
-    Right(n).filterOrElse(_.nonNegative >= 0, "Non a non-negative number")
-  }
+  implicit val naturalCodec: Codec[Natural] =
+    deriveCodec[Natural].iemap(
+      Right(_).filterOrElse(_.nonNegative >= 0, "Non a non-negative number")
+    )(identity)
 
   implicit val naturalFromInternal: FromAndToInternal[Natural, spire.math.Natural] = FromAndToInternal.create(
     fromInternal = n => Natural(n.intValue),
