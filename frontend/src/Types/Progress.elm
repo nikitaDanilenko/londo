f75b5d65d24191a6197cb131bf992550d76e5006
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-05-18 10:35:57 +0200

    #15: Add preliminary display function

--- frontend/src/Types/Progress.elm
+++ frontend/src/Types/Progress.elm
@@ -1,8 +1,10 @@
 module Types.Progress exposing (..)
 
+import List.Extra
+import LondoGQL.Enum.TaskKind as TaskKind exposing (TaskKind)
 import Monocle.Lens exposing (Lens)
-import Types.Natural exposing (Natural)
-import Types.Positive exposing (Positive)
+import Types.Natural as Natural exposing (Natural)
+import Types.Positive as Positive exposing (Positive)
 
 
 type alias Progress =
@@ -19,3 +21,34 @@ reachable =
 reached : Lens Progress Natural
 reached =
     Lens .reached (\b a -> { a | reached = b })
+
+
+display : TaskKind -> Progress -> String
+display tk p =
+    case tk of
+        TaskKind.Percentual ->
+            let
+                numberOfDecimalPlaces =
+                    logBase 10 (toFloat p.reachable.positive) - 2 |> round
+
+                reachedString =
+                    p.reached |> Natural.toString
+
+                reachedStringLength =
+                    String.length reachedString
+
+                percent =
+                    if numberOfDecimalPlaces <= 0 then
+                        reachedString
+
+                    else
+                        let
+                            ( before, after ) =
+                                reachedString |> String.toList |> List.Extra.splitAt (reachedStringLength - numberOfDecimalPlaces)
+                        in
+                        String.concat [ String.fromList before, ".", String.fromList after ]
+            in
+            String.concat [ percent, "%" ]
+
+        _ ->
+            String.concat [ Natural.toString p.reached, "/", Positive.toString p.reachable ]
