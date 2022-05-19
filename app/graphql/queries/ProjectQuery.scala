511be10ac08aea31eac9db791ed4253b5f32f777
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-05-19 09:52:37 +0200

    #15: Add query functions for fetching projects with user association

--- app/graphql/queries/ProjectQuery.scala
+++ app/graphql/queries/ProjectQuery.scala
@@ -4,6 +4,7 @@ import cats.data.{ EitherT, NonEmptySet }
 import cats.effect.IO
 import errors.ServerError
 import graphql.types.FromInternal.syntax._
+import graphql.types.ToInternal.syntax._
 import graphql.types.project.{ Project, ProjectId }
 import graphql.types.task.Progress
 import graphql.{ HasGraphQLServices, HasLoggedInUser }
@@ -28,6 +29,33 @@ trait ProjectQuery extends HasGraphQLServices with HasLoggedInUser {
         .map(ResolvedProject.progress(_).map(_.fromInternal[Progress]))
         .value
     }
+
+  @GraphQLField
+  def ownProjects: Future[Seq[Project]] =
+    withUser(userId =>
+      graphQLServices.projectService
+        .fetchOwn(userId.toInternal)
+        .map(_.map(_.fromInternal[Project]))
+    )
+      .unsafeToFuture()
+
+  @GraphQLField
+  def fetchWithReadAccess: Future[Seq[Project]] =
+    withUser(userId =>
+      graphQLServices.projectService
+        .fetchWithReadAccess(userId.toInternal)
+        .map(_.map(_.fromInternal[Project]))
+    )
+      .unsafeToFuture()
+
+  @GraphQLField
+  def fetchWithWriteAccess: Future[Seq[Project]] =
+    withUser(userId =>
+      graphQLServices.projectService
+        .fetchWithReadAccess(userId.toInternal)
+        .map(_.map(_.fromInternal[Project]))
+    )
+      .unsafeToFuture()
 
   private def validateProjectReadAccess[A](
       projectId: ProjectId
