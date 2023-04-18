7ac96197e2c25c11c4d2335bd5892b47e94519fc
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-19 00:15:11 +0200

    15: Add id file

---
+++ app/db/package.scala
@@ -0,0 +1,27 @@
+import shapeless.tag.@@
+
+import java.util.UUID
+
+package object db {
+
+  sealed trait DashboardTag
+
+  type DashboardId = UUID @@ DashboardTag
+
+  sealed trait ProjectTag
+
+  type ProjectId = UUID @@ ProjectTag
+
+  sealed trait UserTag
+
+  type UserId = UUID @@ UserTag
+
+  sealed trait SessionTag
+
+  type SessionId = UUID @@ SessionTag
+
+  sealed trait PlainTaskTag
+
+  type PlainTaskId = UUID @@ PlainTaskTag
+
+}
