0ff2d9714f41f6d68ab7e5abd9265419db8b33b1
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-19 22:18:05 +0200

    15: Set up mail service as in foodie

--- app/services/email/EmailService.scala
+++ app/services/email/EmailService.scala
@@ -1,15 +1,30 @@
 package services.email
 
-import cats.effect.IO
+import play.api.libs.mailer.{ Email, MailerClient }
 
 import javax.inject.Inject
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
 
-class EmailService @Inject() () {
+class EmailService @Inject() (
+    mailerClient: MailerClient
+)(implicit
+    executionContext: ExecutionContext
+) {
 
-  // TODO: Add proper implementation instead of place holder
-  def sendEmail(emailParameters: EmailParameters): IO[Unit] =
-    IO {
-      pprint.log(emailParameters)
-    }
+  private val mailConfig = MailConfiguration.default
+
+  def sendEmail(emailParameters: EmailParameters): Future[Unit] = {
+    val email = Email(
+      subject = emailParameters.subject,
+      from = mailConfig.from,
+      to = emailParameters.to,
+      cc = emailParameters.cc,
+      bodyText = Some(emailParameters.message)
+    )
+    Future {
+      Try(mailerClient.send(email))
+    }.flatMap(_.fold(Future.failed, _ => Future.unit))
+  }
 
 }
