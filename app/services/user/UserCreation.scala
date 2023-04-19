70782a22e06e7a9e3b2acf2d02afa4c3886947ca
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-19 16:34:35 +0200

    15: Adjust user (creation)

--- app/services/user/UserCreation.scala
+++ app/services/user/UserCreation.scala
@@ -1,42 +1,38 @@
 package services.user
 
 import cats.effect.IO
+import db.UserId
+import io.scalaland.chimney.dsl._
 import security.Hash
 import spire.math.Natural
 import utils.random.RandomGenerator
+import utils.transformer.implicits._
 
 case class UserCreation(
     nickname: String,
-    email: String,
     password: String,
-    token: String
+    displayName: Option[String],
+    email: String
 )
 
 object UserCreation {
 
-  val saltLength: Natural = Natural(40)
-  val kdfIterations: Natural = Natural(120000)
+  private val saltLength: Natural = Natural(40)
 
-  def create(userCreation: UserCreation): IO[CreatedUser] =
+  def create(userCreation: UserCreation): IO[User] =
     for {
-      id <- RandomGenerator.randomUUID
+      id   <- RandomGenerator.randomUUID
       salt <- RandomGenerator.randomString(saltLength)
-    } yield CreatedUser(
-      User(
-        id = UserId(id),
-        nickname = userCreation.nickname,
-        email = userCreation.email,
-        settings = UserSettings.default,
-        details = UserDetails.default
-      ),
-      passwordParameters = PasswordParameters(
-        salt = salt,
-        hash = Hash.fromPassword(
-          password = userCreation.password,
-          salt = salt,
-          iterations = kdfIterations
-        ),
-        iterations = kdfIterations
+    } yield User(
+      id = id.transformInto[UserId],
+      nickname = userCreation.nickname,
+      displayName = userCreation.displayName,
+      email = userCreation.email,
+      salt = salt,
+      hash = Hash.fromPassword(
+        userCreation.password,
+        salt,
+        Hash.defaultIterations
       )
     )
 
