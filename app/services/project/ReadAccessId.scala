2639682089299f18e0831202b916715fa07f1c37
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-21 12:43:39 +0200

    15: Remove access components

--- app/services/project/ReadAccessId.scala
+++
@@ -1,5 +0,0 @@
-package services.project
-
-import java.util.UUID
-
-case class ReadAccessId(uuid: UUID) extends AnyVal
--- app/services/project/WriteAccessId.scala
+++
@@ -1,5 +0,0 @@
-package services.project
-
-import java.util.UUID
-
-case class WriteAccessId(uuid: UUID) extends AnyVal
