549dca77aa096215bc4a49fcbbcc0bd818c4417a
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-22 00:36:06 +0200

    15: Make JWT creation more generic

--- app/utils/jwt/JwtUtil.scala
+++ app/utils/jwt/JwtUtil.scala
@@ -1,12 +1,11 @@
 package utils.jwt
 
 import errors.{ ErrorContext, ServerError }
-import graphql.types.FromInternal.syntax._
+import io.circe.Encoder
 import io.circe.syntax._
 import pdi.jwt.algorithms.JwtAsymmetricAlgorithm
 import pdi.jwt.{ JwtAlgorithm, JwtCirce, JwtClaim, JwtHeader }
 import security.jwt.{ JwtContent, JwtExpiration }
-import services.user.{ SessionId, UserId }
 
 object JwtUtil {
 
@@ -25,14 +24,11 @@ object JwtUtil {
           .map(_ => ErrorContext.Authentication.Token.Content.asServerError)
       }
 
-  def createJwt(userId: UserId, sessionId: SessionId, privateKey: String, expiration: JwtExpiration): String =
+  def createJwt[A: Encoder](content: A, privateKey: String, expiration: JwtExpiration): String =
     JwtCirce.encode(
       header = JwtHeader(algorithm = signatureAlgorithm),
       claim = JwtClaim(
-        content = JwtContent(
-          userId = userId.fromInternal,
-          sessionId = sessionId.fromInternal
-        ).asJson.noSpaces,
+        content = content.asJson.noSpaces,
         expiration = expiration.expirationAt,
         notBefore = expiration.notBefore
       ),
