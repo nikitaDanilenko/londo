1b71f188e612d69ed4a048d02e9747a93dc07fdf
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-22 00:35:59 +0200

    15: Remove date function

--- app/utils/DBIOUtil.scala
+++ app/utils/DBIOUtil.scala
@@ -1,13 +1,8 @@
 package utils
 
-import cats.syntax.contravariantSemigroupal._
 import cats.{ Monad, StackSafeMonad }
-import io.scalaland.chimney.dsl._
 import slick.dbio.DBIO
-import slick.jdbc.PostgresProfile.api._
-import slick.lifted.Rep
 
-import java.time.LocalDate
 import scala.concurrent.ExecutionContext
 
 object DBIOUtil {
@@ -21,20 +16,6 @@ object DBIOUtil {
         override def pure[A](x: A): DBIO[A] = DBIO.successful(x)
       }
 
-  }
-
-  // TODO: Check use
-  def dateFilter(from: Option[LocalDate], to: Option[LocalDate]): Rep[java.sql.Date] => Rep[Boolean] = {
-    val startFilter: LocalDate => Rep[java.sql.Date] => Rep[Boolean] = start => _ >= start.transformInto[java.sql.Date]
-
-    val endFilter: LocalDate => Rep[java.sql.Date] => Rep[Boolean] = end => _ <= end.transformInto[java.sql.Date]
-
-    (from, to) match {
-      case (Some(start), Some(end)) => (startFilter(start), endFilter(end)).mapN(_ && _)
-      case (Some(start), _)         => startFilter(start)
-      case (_, Some(end))           => endFilter(end)
-      case _                        => _ => true
-    }
   }
 
 }
