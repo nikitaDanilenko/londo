3386d92689943fede8febadc620424eeccaf3205
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-22 00:53:15 +0200

    15: Add project transformer

--- app/services/project/Project.scala
+++ app/services/project/Project.scala
@@ -3,8 +3,11 @@ package services.project
 import db.generated.Tables
 import db.{ ProjectId, UserId }
 import io.scalaland.chimney.Transformer
+import io.scalaland.chimney.dsl._
+import utils.transformer.implicits._
 
 import java.time.LocalDateTime
+import java.util.UUID
 
 case class Project(
     id: ProjectId,
@@ -15,6 +18,22 @@ case class Project(
 )
 
 object Project {
-  implicit val fromDB: Transformer[Tables.ProjectRow, Project]         = ???
-  implicit val toDB: Transformer[(Project, UserId), Tables.ProjectRow] = ???
+
+  implicit val fromDB: Transformer[Tables.ProjectRow, Project] =
+    Transformer
+      .define[Tables.ProjectRow, Project]
+      .buildTransformer
+
+  implicit val toDB: Transformer[(Project, UserId), Tables.ProjectRow] = { case (project, ownerId) =>
+    Tables.ProjectRow(
+      id = project.id.transformInto[UUID],
+      ownerId = ownerId.transformInto[UUID],
+      name = project.name,
+      description = project.description,
+      createdAt = project.createdAt.transformInto[java.sql.Timestamp],
+      updatedAt = project.updatedAt.map(_.transformInto[java.sql.Timestamp])
+    )
+
+  }
+
 }
