422c16a6da368ffa08e40f075cc67b4500d656fa
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-22 01:03:00 +0200

    15: Add task transformer

--- app/services/task/Task.scala
+++ app/services/task/Task.scala
@@ -3,8 +3,13 @@ package services.task
 import db.{ ProjectId, TaskId }
 import db.generated.Tables
 import io.scalaland.chimney.Transformer
+import io.scalaland.chimney.dsl._
+import math.Positive
+import spire.math.Natural
+import utils.transformer.implicits._
 
 import java.time.LocalDateTime
+import java.util.UUID
 
 case class Task(
     id: TaskId,
@@ -18,6 +23,38 @@ case class Task(
 )
 
 object Task {
-  implicit val fromDB: Transformer[Tables.TaskRow, Task]            = ???
-  implicit val toDB: Transformer[(Task, ProjectId), Tables.TaskRow] = ???
+
+  implicit val fromDB: Transformer[Tables.TaskRow, Task] = row =>
+    Task(
+      id = row.id.transformInto[TaskId],
+      name = row.name,
+      taskKind = TaskKind.withName(row.kind),
+      unit = row.unit,
+      progress = Progress.fraction(
+        reachable = Positive.nextOf(
+          Natural(row.reachable)
+        ),
+        reached = Natural(row.reached)
+      ),
+      counting = row.counting,
+      createdAt = row.createdAt.transformInto[LocalDateTime],
+      updatedAt = row.updatedAt.map(_.transformInto[LocalDateTime])
+    )
+
+  implicit val toDB: Transformer[(Task, ProjectId), Tables.TaskRow] = { case (task, projectId) =>
+    Tables.TaskRow(
+      id = task.id.transformInto[UUID],
+      projectId = projectId.transformInto[UUID],
+      name = task.name,
+      unit = task.unit,
+      kind = task.taskKind.entryName,
+      reached = task.progress.reached.longValue,
+      reachable = task.progress.reachable.natural.longValue,
+      counting = task.counting,
+      createdAt = task.createdAt.transformInto[java.sql.Timestamp],
+      updatedAt = task.updatedAt.map(_.transformInto[java.sql.Timestamp])
+    )
+
+  }
+
 }
