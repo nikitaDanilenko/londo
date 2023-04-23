80e301b6fc3d20acbe595f477819d8aa7af4ef82
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-23 09:26:28 +0200

    15: Add logout mode

---
+++ app/graphql/types/user/LogoutMode.scala
@@ -0,0 +1,22 @@
+package graphql.types.user
+
+import enumeratum.{ CirceEnum, Enum, EnumEntry }
+import graphql.types.task.TaskKind
+import io.circe.Json
+import sangria.macros.derive.deriveEnumType
+import sangria.marshalling.ToInput
+import sangria.marshalling.circe.circeEncoderToInput
+import sangria.schema.EnumType
+
+sealed trait LogoutMode extends EnumEntry
+
+object LogoutMode extends Enum[LogoutMode] with CirceEnum[LogoutMode] {
+  case object ThisSession extends LogoutMode
+  case object AllSessions extends LogoutMode
+
+  override lazy val values: IndexedSeq[LogoutMode] = findValues
+
+  implicit val toInput: ToInput[LogoutMode, Json] = circeEncoderToInput[LogoutMode]
+
+  implicit val enumType: EnumType[LogoutMode] = deriveEnumType[LogoutMode]()
+}
