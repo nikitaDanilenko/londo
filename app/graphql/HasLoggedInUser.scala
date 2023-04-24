fca517bf5e4d0b47241209103f835a002d846877
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-25 00:05:51 +0200

    15: Add utility function

--- app/graphql/HasLoggedInUser.scala
+++ app/graphql/HasLoggedInUser.scala
@@ -3,7 +3,11 @@ package graphql
 import cats.syntax.flatMap._
 import cats.{ ApplicativeThrow, MonadThrow }
 import errors.{ ErrorContext, ServerException }
+import io.scalaland.chimney.dsl._
 import security.jwt.LoggedIn
+import utils.transformer.implicits._
+
+import scala.util.chaining._
 
 trait HasLoggedInUser {
   protected def loggedIn: Option[LoggedIn]
@@ -15,5 +19,8 @@ trait HasLoggedInUser {
         ServerException(ErrorContext.Authentication.Token.Restricted.asServerError)
       )
       .flatMap(action)
+
+  final protected def withUserId[F[_]: MonadThrow, A](action: db.UserId => F[A]): F[A] =
+    withUser(_.userId.transformInto[db.UserId].pipe(action))
 
 }
