d0226abc1a4fc2763b8719c2f208becd1b2a8131
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-30 12:15:47 +0200

    15: Extend links

--- frontend/src/Pages/Util/Links.elm
+++ frontend/src/Pages/Util/Links.elm
@@ -1,20 +1,59 @@
 module Pages.Util.Links exposing (..)
 
+import Addresses.Frontend
+import Basics.Extra exposing (flip)
 import Bootstrap.Button
-import Html exposing (Attribute, Html)
+import Configuration exposing (Configuration)
+import Html exposing (Attribute, Html, text)
 import Html.Attributes exposing (href)
+import Loading
+import Url.Builder
 
 
 linkButton :
     { url : String
     , attributes : List (Attribute msg)
     , children : List (Html msg)
-    , isDisabled : Bool
     }
     -> Html msg
 linkButton params =
     Bootstrap.Button.linkButton
-        [ Bootstrap.Button.disabled params.isDisabled
-        , Bootstrap.Button.attrs (href params.url :: params.attributes)
+        [ Bootstrap.Button.attrs (href params.url :: params.attributes)
         ]
         params.children
+
+
+special : Int -> String
+special =
+    Char.fromCode >> String.fromChar
+
+
+lookingGlass : String
+lookingGlass =
+    special 128269
+
+
+loadingSymbol : Html msg
+loadingSymbol =
+    Loading.render Loading.Spinner Loading.defaultConfig Loading.On
+
+
+frontendPage : Configuration -> List String -> String
+frontendPage configuration pathSteps =
+    [ configuration.mainPageURL, "#" ]
+        ++ pathSteps
+        |> flip Url.Builder.relative []
+
+
+toLoginButtonWith :
+    { configuration : Configuration
+    , buttonText : String
+    , attributes : List (Attribute msg)
+    }
+    -> Html msg
+toLoginButtonWith params =
+    linkButton
+        { url = frontendPage params.configuration <| Addresses.Frontend.login.address ()
+        , attributes = params.attributes
+        , children = [ text <| params.buttonText ]
+        }
