ec7e191c79b38ad11e1b9c639ada0d9e0cd1751b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-30 22:47:10 +0200

    15: Add view utility module

--- frontend/src/Pages/Util/Style.elm
+++ frontend/src/Pages/Util/Style.elm
@@ -9,8 +9,11 @@ classes :
         { confirm : Attribute msg
         , error : Attribute msg
         , navigation : Attribute msg
+        , pager : Attribute msg
         }
+    , disabled : Attribute msg
     , editable : Attribute msg
+    , ellipsis : Attribute msg
     , info : Attribute msg
     }
 classes =
@@ -18,19 +21,24 @@ classes =
         { confirm = class "confirm-button"
         , error = class "error-button"
         , navigation = class "navigation-button"
+        , pager = class "pager-button"
         }
+    , disabled = class "disabled"
     , editable = class "editable"
+    , ellipsis = class "ellipsis"
     , info = class "info"
     }
 
 
 ids :
     { error : Attribute msg
+    , navigation : Attribute msg
     , registrationRequestSent : Attribute msg
     , requestRegistration : Attribute msg
     }
 ids =
     { error = id "error"
+    , navigation = id "navigation"
     , registrationRequestSent = id "registration-request-sent"
     , requestRegistration = id "request-registration"
     }
