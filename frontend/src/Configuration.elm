1b1fc00d404ed76aac2cba931e739b9bc69c2c65
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-04-30 11:11:13 +0200

    15: Add routes, propagate simpler configuration

--- frontend/src/Configuration.elm
+++ frontend/src/Configuration.elm
@@ -1,21 +1,7 @@
 module Configuration exposing (..)
 
---todo: Keep watch for possible restructuring; this may be sensible to provide only the relevant configuration flags
-
 
 type alias Configuration =
     { graphQLEndpoint : String
     , mainPageURL : String
-    , subFolders : SubFolders
-    }
-
-
-type alias SubFolders =
-    { register : String
-    , login : String
-    , overview : String
-    , dashboards : String
-    , projects : String
-    , tasks : String
-    , settings : String
     }
