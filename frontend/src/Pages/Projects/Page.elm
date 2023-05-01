e5527575c7d812cfcd1dc989147d4c0631dc4be6
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-05-01 23:39:28 +0200

    15: Reference types

--- frontend/src/Pages/Projects/Page.elm
+++ frontend/src/Pages/Projects/Page.elm
@@ -1,12 +1,12 @@
 module Pages.Projects.Page exposing (..)
 
-import Pages.Projects.Creation
-import Pages.Projects.Project
-import Pages.Projects.Update
 import Pages.Util.AuthorizedAccess exposing (AuthorizedAccess)
 import Pages.Util.ParentEditor.Page
 import Pages.View.Tristate as Tristate
-import Types.ProjectId exposing (ProjectId)
+import Types.Project.Creation
+import Types.Project.Project
+import Types.Project.ProjectId
+import Types.Project.Update
 import Util.DictList exposing (DictList)
 import Util.Editing exposing (Editing)
 
@@ -15,29 +15,36 @@ type alias Model =
     Tristate.Model Main Initial
 
 
+type alias Id =
+    Types.Project.ProjectId.ProjectId
+
+
+type alias Project =
+    Types.Project.Project.Project
+
+
+type alias Creation =
+    Types.Project.Creation.ClientInput
+
+
+type alias Update =
+    Types.Project.Update.ClientInput
+
+
 type alias Main =
-    Pages.Util.ParentEditor.Page.Main
-        ProjectId
-        Pages.Projects.Project.Project
-        Pages.Projects.Creation.ClientInput
-        Pages.Projects.Update.ClientInput
+    Pages.Util.ParentEditor.Page.Main Id Project Creation Update
 
 
 type alias Initial =
-    Pages.Util.ParentEditor.Page.Initial
-        ProjectId
-        Pages.Projects.Project.Project
-        Pages.Projects.Update.ClientInput
+    Pages.Util.ParentEditor.Page.Initial Id Project Update
 
 
 type alias ProjectState =
-    Editing
-        Pages.Projects.Project.Project
-        Pages.Projects.Update.ClientInput
+    Editing Project Update
 
 
 type alias ProjectStateMap =
-    DictList ProjectId ProjectState
+    DictList Id ProjectState
 
 
 type alias Flags =
@@ -50,8 +57,4 @@ type alias Msg =
 
 
 type alias LogicMsg =
-    Pages.Util.ParentEditor.Page.LogicMsg
-        ProjectId
-        Pages.Projects.Project.Project
-        Pages.Projects.Creation.ClientInput
-        Pages.Projects.Update.ClientInput
+    Pages.Util.ParentEditor.Page.LogicMsg Id Project Creation Update
