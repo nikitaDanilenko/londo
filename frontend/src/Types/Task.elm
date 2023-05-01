4477c9d2850dbd735f711576ffe18ee12a6caa5b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-05-01 16:55:15 +0200

    15: Rename task

---
+++ frontend/src/Types/Task.elm
@@ -0,0 +1,29 @@
+module Types.Task exposing (..)
+
+import GraphQLFunctions.OptionalArgumentUtil as OptionalArgumentUtil
+import LondoGQL.Enum.TaskKind exposing (TaskKind)
+import LondoGQL.InputObject exposing (TaskCreation, TaskUpdate)
+import Types.Positive exposing (Positive)
+import Types.Progress exposing (Progress)
+import Types.TaskId exposing (TaskId)
+
+
+type alias Task =
+    { id : TaskId
+    , name : String
+    , taskKind : TaskKind
+    , unit : Maybe String
+    , progress : Progress
+    , counting: Bool
+    }
+
+
+fromCreation : TaskId -> TaskCreation -> Task
+fromCreation id taskCreation =
+    { id = id
+    , name = taskCreation.name
+    , taskKind = taskCreation.taskKind
+    , unit = OptionalArgumentUtil.toMaybe taskCreation.unit
+    , progress = taskCreation.progress
+    , counting = taskCreation.counting
+    }
