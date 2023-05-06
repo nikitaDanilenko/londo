2b050bb1e7086dea822df064fbbefac2b271abc9
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-05-06 12:52:57 +0200

    15: Propagate renaming

--- frontend/src/Types/Task/Creation.elm
+++ frontend/src/Types/Task/Creation.elm
@@ -3,23 +3,34 @@ module Types.Task.Creation exposing (..)
 import Graphql.OptionalArgument as OptionalArgument
 import LondoGQL.Enum.TaskKind exposing (TaskKind)
 import LondoGQL.InputObject exposing (ProgressInput)
-import Types.Task.Progress exposing (Progress)
+import Util.ValidatedInput as ValidatedInput exposing (ValidatedInput)
+import Types.Progress.Input
 
 
 type alias ClientInput =
-    { name : String
+    { name : ValidatedInput String
     , taskKind : TaskKind
     , unit : Maybe String
-    , progress : Types.Task.Progress.ClientInput
+    , progress : Types.Progress.Input.ClientInput
     , counting : Bool
+    }
+
+
+default : ClientInput
+default =
+    { name = ValidatedInput.nonEmptyString
+    , taskKind = LondoGQL.Enum.TaskKind.Percentual
+    , unit = Nothing
+    , progress = Types.Progress.Input.default
+    , counting = True
     }
 
 
 toGraphQLInput : ClientInput -> LondoGQL.InputObject.TaskCreation
 toGraphQLInput clientInput =
-    { name = clientInput.name
+    { name = clientInput.name.value
     , taskKind = clientInput.taskKind
     , unit = clientInput.unit |> OptionalArgument.fromMaybe
-    , progress = clientInput.progress |> Types.Task.Progress.toGraphQLInput
+    , progress = clientInput.progress |> Types.Progress.Input.toGraphQLInput
     , counting = clientInput.counting
     }
--- frontend/src/Types/Task/Update.elm
+++ frontend/src/Types/Task/Update.elm
@@ -7,13 +7,13 @@ import LondoGQL.InputObject
 import LondoGQL.Mutation
 import Monocle.Lens exposing (Lens)
 import Pages.Util.AuthorizedAccess exposing (AuthorizedAccess)
-import Pages.Util.ValidatedInput as ValidatedInput exposing (ValidatedInput)
-import Types.Progress.Update
+import Types.Progress.Input
 import Types.Project.ProjectId
 import Types.Task.Task
 import Types.Task.TaskId
 import Types.Task.Update
 import Util.HttpUtil as HttpUtil
+import Util.ValidatedInput as ValidatedInput exposing (ValidatedInput)
 
 
 type alias ClientInput =
@@ -21,7 +21,7 @@ type alias ClientInput =
     , taskKind : TaskKind
     , unit : Maybe String
     , counting : Bool
-    , progressUpdate : Types.Progress.Update.ClientInput
+    , progressUpdate : Types.Progress.Input.ClientInput
     }
 
 
@@ -30,7 +30,7 @@ lenses :
     , taskKind : Lens ClientInput TaskKind
     , unit : Lens ClientInput (Maybe String)
     , counting : Lens ClientInput Bool
-    , progressUpdate : Lens ClientInput Types.Progress.Update.ClientInput
+    , progressUpdate : Lens ClientInput Types.Progress.Input.ClientInput
     }
 lenses =
     { name = Lens .name (\b a -> { a | name = b })
@@ -50,7 +50,7 @@ from task =
     , taskKind = task.taskKind
     , unit = task.unit
     , counting = task.counting
-    , progressUpdate = Types.Progress.Update.from task.progress
+    , progressUpdate = Types.Progress.Input.from task.progress
     }
 
 
@@ -60,7 +60,7 @@ toGraphQLInput clientInput =
     , taskKind = clientInput.taskKind
     , unit = clientInput.unit |> OptionalArgument.fromMaybe
     , counting = clientInput.counting
-    , progressUpdate = clientInput.progressUpdate |> Types.Progress.Update.toGraphQLInput
+    , progressUpdate = clientInput.progressUpdate |> Types.Progress.Input.toGraphQLInput
     }
 
 
