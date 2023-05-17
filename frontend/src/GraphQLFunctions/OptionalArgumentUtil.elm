02f3ac94ccd260fad5676dfa6a7959dff3e1d457
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-05-17 11:13:37 +0200

    Remove unused types, and functions

--- frontend/src/GraphQLFunctions/OptionalArgumentUtil.elm
+++ frontend/src/GraphQLFunctions/OptionalArgumentUtil.elm
@@ -3,6 +3,10 @@ module GraphQLFunctions.OptionalArgumentUtil exposing (..)
 import Graphql.OptionalArgument exposing (OptionalArgument(..))
 
 
+
+-- todo: Check uses
+
+
 toMaybe : OptionalArgument a -> Maybe a
 toMaybe arg =
     case arg of
