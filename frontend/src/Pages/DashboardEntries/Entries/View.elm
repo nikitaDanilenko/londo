665f5f74708ec1a7c15ccde5e2297ee80e2305cc
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-07-06 21:06:51 +0200

    Start adding view implementations

--- frontend/src/Pages/DashboardEntries/Entries/View.elm
+++ frontend/src/Pages/DashboardEntries/Entries/View.elm
@@ -1,9 +1,21 @@
 module Pages.DashboardEntries.Entries.View exposing (..)
 
 import Configuration exposing (Configuration)
-import Html exposing (Html)
+import Html exposing (Html, button, label, td, text, th)
+import Html.Events exposing (onClick)
+import Maybe.Extra
 import Pages.DashboardEntries.Entries.Page as Page
+import Pages.Projects.View
+import Pages.Util.Choice.Page
 import Pages.Util.Choice.View
+import Pages.Util.HtmlUtil as HtmlUtil
+import Pages.Util.NavigationUtil as NavigationUtil
+import Pages.Util.Style as Style
+import Types.DashboardEntry.Creation
+import Types.Project.Id
+import Types.Project.Project
+import Util.DictList as DictList exposing (DictList)
+import Util.Editing exposing (Editing)
 import Util.SearchUtil as SearchUtil
 
 
@@ -13,11 +25,18 @@ viewEntries configuration main =
         { nameOfChoice = .name
         , choiceIdOfElement = .projectId
         , idOfElement = .projectId
-        , elementHeaderColumns = []
+        , elementHeaderColumns =
+            [ th [] [ label [] [ text <| .dashboardEntryName <| main.language ] ]
+            , th [] [ label [] [ text <| .dashboardEntryDescription <| main.language ] ]
+            ]
         , info =
             \entry ->
-                { display = []
-                , controls = []
+                { display = projectInfoFromMap main.choices entry.projectId
+                , controls =
+                    [ td [ Style.classes.controls ] [ button [ Style.classes.button.edit, onClick <| Pages.Util.Choice.Page.EnterEdit <| entry.projectId ] [ text <| .edit <| main.language ] ]
+                    , td [ Style.classes.controls ] [ button [ Style.classes.button.delete, onClick <| Pages.Util.Choice.Page.RequestDelete <| entry.projectId ] [ text <| .delete <| main.language ] ]
+                    , td [ Style.classes.controls ] [ NavigationUtil.projectEditorLinkButton configuration entry.projectId <| .taskEditor <| main.language ]
+                    ]
                 }
         , isValidInput = always True
         , edit =
@@ -41,8 +60,29 @@ viewProjects configuration main =
                 }
         , viewChoiceLine =
             \project ->
-                { display = []
-                , controls = []
+                { display =
+                    Pages.Projects.View.projectInfoColumns project
+                        ++ [ { attributes = [ Style.classes.editable, Style.classes.numberCell ]
+                             , children = []
+                             }
+                           ]
+                , controls =
+                    [ td [ Style.classes.controls ]
+                        [ button [ Style.classes.button.select, onClick <| Pages.Util.Choice.Page.SelectChoice <| project ]
+                            [ text <| .select <| .language <| main ]
+                        ]
+                    , td [ Style.classes.controls ]
+                        [ NavigationUtil.projectEditorLinkButton configuration project.id main.language.projects ]
+                    ]
                 }
         }
         main
+
+
+{-| Todo: The function is oddly specific, and the implementation with the fixed amount of columns is awkward,
+especially because the non-matching case should never occur.
+-}
+projectInfoFromMap : DictList Types.Project.Id.Id (Editing Types.Project.Project.Project Types.DashboardEntry.Creation.ClientInput) -> Types.Project.Id.Id -> List (HtmlUtil.Column Page.LogicMsg)
+projectInfoFromMap projects projectId =
+    DictList.get projectId projects
+        |> Maybe.Extra.unwrap (List.repeat 2 { attributes = [ Style.classes.editable ], children = [] }) (.original >> Pages.Projects.View.projectInfoColumns)
