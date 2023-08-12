c638b1618daeb76c1f399059fa01c354538a1ca1
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-08-12 14:31:41 +0200

    Add statistics functions

--- frontend/src/Math/Statistics.elm
+++ frontend/src/Math/Statistics.elm
@@ -4,6 +4,8 @@ import Basics.Extra exposing (flip)
 import BigInt exposing (BigInt)
 import BigRational exposing (BigRational)
 import Math.Constants
+import Math.Positive as Positive
+import Maybe.Extra
 import Types.Progress.Progress
 
 
@@ -35,3 +37,30 @@ relativeRounded divisor =
         Math.Constants.zeroBigInt
         >> BigRational.fromBigInt
         >> flip BigRational.div (divisor |> BigRational.fromInt)
+
+
+{-| The difference if one additional reachable point is added is
+100 / (n \* reachable)
+where n is the number of all tasks.
+-}
+differenceAfterOneMoreExact : { numberOfElements : Int } -> Types.Progress.Progress.Progress -> Maybe BigRational
+differenceAfterOneMoreExact ps =
+    Just
+        >> Maybe.Extra.filter (Types.Progress.Progress.isComplete >> not)
+        >> Maybe.map
+            (.reachable
+                >> Positive.integerValue
+                >> BigInt.mul (ps.numberOfElements |> BigInt.fromInt)
+                >> BigRational.fromBigInts Math.Constants.oneHundredBigInt
+            )
+
+
+differenceAfterCompletionExact : { numberOfElements : Int } -> Types.Progress.Progress.Progress -> Maybe BigRational
+differenceAfterCompletionExact ps =
+    Just
+        >> Maybe.Extra.filter (Types.Progress.Progress.isComplete >> not)
+        >> Maybe.map
+            (Types.Progress.Progress.toRational
+                >> flip BigRational.sub (BigRational.fromInt 1)
+                >> BigRational.mul (BigRational.fromBigInts Math.Constants.oneHundredBigInt (ps.numberOfElements |> BigInt.fromInt))
+            )
