801233c54db4a2fae0f322a9dedd924c7b8436e3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-08-17 23:53:18 +0200

    Add function for computing rounded difference after one more reached

--- frontend/src/Math/Statistics.elm
+++ frontend/src/Math/Statistics.elm
@@ -4,6 +4,7 @@ import Basics.Extra exposing (flip)
 import BigInt exposing (BigInt)
 import BigRational exposing (BigRational)
 import Math.Constants
+import Math.Natural as Natural
 import Math.Positive as Positive
 import Maybe.Extra
 import Types.Progress.Progress
@@ -52,6 +53,52 @@ differenceAfterOneMoreExact ps =
                 >> Positive.integerValue
                 >> BigInt.mul (ps.numberOfElements |> BigInt.fromInt)
                 >> BigRational.fromBigInts Math.Constants.oneHundredBigInt
+            )
+
+
+{-| The difference if one additional reachable point is added is
+(1 / n) \* (floor(100 \* (1 + reached) / reachable) - floor(100 \* reached / reachable))
+where n is the number of all tasks.
+-}
+differenceAfterOneMoreFloored : { numberOfElements : Int } -> Types.Progress.Progress.Progress -> Maybe BigRational
+differenceAfterOneMoreFloored ps progress =
+    let
+        reached =
+            progress.reached |> Natural.integerValue
+
+        reachable =
+            progress.reachable |> Positive.integerValue
+
+        current =
+            BigRational.fromBigInts
+                (BigInt.mul
+                    Math.Constants.oneHundredBigInt
+                    reached
+                )
+                reachable
+
+        assumed =
+            BigRational.fromBigInts
+                (BigInt.mul
+                    Math.Constants.oneHundredBigInt
+                    (BigInt.add
+                        Math.Constants.oneBigInt
+                        reached
+                    )
+                )
+                reachable
+    in
+    progress
+        |> Just
+        |> Maybe.Extra.filter (Types.Progress.Progress.isComplete >> not)
+        |> Maybe.map
+            (\_ ->
+                BigRational.fromBigInts
+                    (BigInt.sub
+                        (assumed |> BigRational.floor)
+                        (current |> BigRational.floor)
+                    )
+                    (ps.numberOfElements |> BigInt.fromInt)
             )
 
 
