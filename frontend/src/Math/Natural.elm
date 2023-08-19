d88bdac1a9adff081136e478041bcb868e89a618
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-08-19 17:21:45 +0200

    Add utility functions

--- frontend/src/Math/Natural.elm
+++ frontend/src/Math/Natural.elm
@@ -1,4 +1,4 @@
-module Math.Natural exposing (Natural, fromPositive, fromString, integerValue, min, one, selection, sum, toGraphQLInput, toString, zero)
+module Math.Natural exposing (Natural, fromBigIntOrZero, fromPositive, fromString, integerValue, min, one, selection, sum, toGraphQLInput, toString, zero)
 
 import BigInt exposing (BigInt)
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
@@ -54,6 +54,15 @@ one : Natural
 one =
     Constants.oneBigInt
         |> NonNegative
+
+
+fromBigIntOrZero : BigInt -> Natural
+fromBigIntOrZero bi =
+    if BigInt.lt bi Constants.zeroBigInt then
+        one
+
+    else
+        NonNegative bi
 
 
 min : Natural -> Natural -> Natural
