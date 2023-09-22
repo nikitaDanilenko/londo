d3e6ad4385663a58bc6048342924a722805964ed
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-09-22 22:54:19 +0200

    Use semantic elements for search area definition

--- frontend/src/Pages/Util/HtmlUtil.elm
+++ frontend/src/Pages/Util/HtmlUtil.elm
@@ -1,7 +1,7 @@
 module Pages.Util.HtmlUtil exposing (Column, RowWithControls, Structure, menuIcon, onEscape, searchAreaWith, toggleControlsCell, withAttributes, withExtraAttributes)
 
-import Html exposing (Attribute, Html, button, div, input, label, td, text)
-import Html.Attributes exposing (disabled, value)
+import Html exposing (Attribute, Html, button, form, input, label, node, td, text)
+import Html.Attributes exposing (disabled, type_, value)
 import Html.Events exposing (on, onClick, onInput)
 import Keyboard.Event exposing (KeyboardEvent)
 import Keyboard.Key as Key
@@ -18,20 +18,27 @@ searchAreaWith :
     }
     -> Html msg
 searchAreaWith ps =
-    div [ Style.classes.search.area ]
-        [ label [] [ text Links.lookingGlass ]
-        , input
-            [ onInput <| ps.msg
-            , value <| ps.searchString
-            , Style.classes.search.field
-            ]
+    node "search"
+        []
+        [ form
             []
-        , button
-            [ Style.classes.button.cancel
-            , onClick <| ps.msg ""
-            , disabled <| String.isEmpty <| ps.searchString
+            [ label [ Style.classes.search.field ] [ text Links.lookingGlass ]
+            , input
+                [ type_ "text"
+                , Style.ids.searchField
+                , onInput <| ps.msg
+                , value <| ps.searchString
+                ]
+                []
+            , input
+                [ Style.classes.button.cancel
+                , value "Clear" -- todo: Use language element here
+                , type_ "reset"
+                , onClick <| ps.msg ""
+                , disabled <| String.isEmpty <| ps.searchString
+                ]
+                []
             ]
-            [ text "Clear" ]
         ]
 
 
