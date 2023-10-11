2516cfdebe36643ccf7a8f799689d0d475311837
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-10-11 14:11:24 +0200

    Adjust keys

--- conf/db/migration/default/V4__add_simulation.sql
+++ conf/db/migration/default/V4__add_simulation.sql
@@ -1,11 +1,13 @@
 create table simulation
 (
     task_id          uuid not null,
+    project_id       uuid not null,
     dashboard_id     uuid not null,
     reached_modifier int  not null
 );
 
 alter table simulation
     add constraint simulation_pk primary key (task_id, dashboard_id),
-    add constraint simulation_task_id_fk foreign key (task_id) references task (id) on delete cascade,
+    add constraint simulation_project_id_fk foreign key (project_id) references project(id) on delete cascade,
+    add constraint simulation_task_id_fk foreign key (task_id, project_id) references task (id, project_id) on delete cascade,
     add constraint simulation_dashboard_id_fk foreign key (dashboard_id) references dashboard (id) on delete cascade;
