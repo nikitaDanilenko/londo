530c9e007b223691b20c10add4591f7b98d42096
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-10-12 00:37:35 +0200

    Add lenses

--- frontend/src/Types/Task/Resolved.elm
+++ frontend/src/Types/Task/Resolved.elm
@@ -3,6 +3,7 @@ module Types.Task.Resolved exposing (..)
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
 import LondoGQL.Object
 import LondoGQL.Object.ResolvedTask
+import Monocle.Lens exposing (Lens)
 import Types.Simulation.Simulation exposing (Simulation)
 import Types.Task.Task exposing (Task)
 
@@ -10,6 +11,20 @@ import Types.Task.Task exposing (Task)
 type alias Resolved =
     { task : Task
     , simulation : Maybe Simulation
+    }
+
+
+
+--todo: Check usage of lenses
+
+
+lenses :
+    { task : Lens Resolved Task
+    , simulation : Lens Resolved (Maybe Simulation)
+    }
+lenses =
+    { task = Lens .task (\b a -> { a | task = b })
+    , simulation = Lens .simulation (\b a -> { a | simulation = b })
     }
 
 
