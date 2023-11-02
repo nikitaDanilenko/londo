6fa9a7955bfe2863da682683e574b2d9f61d5f53
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-11-02 23:49:04 +0100

    Extend deeply resolved dashboard type

--- frontend/src/Types/Dashboard/DeeplyResolved.elm
+++ frontend/src/Types/Dashboard/DeeplyResolved.elm
@@ -7,6 +7,7 @@ import LondoGQL.Query
 import Pages.Util.AuthorizedAccess exposing (AuthorizedAccess)
 import Types.Dashboard.Dashboard
 import Types.Dashboard.Id
+import Types.Dashboard.Statistics
 import Types.Project.DeeplyResolved
 import Util.HttpUtil as HttpUtil
 
@@ -14,15 +15,17 @@ import Util.HttpUtil as HttpUtil
 type alias DeeplyResolved =
     { dashboard : Types.Dashboard.Dashboard.Dashboard
     , resolvedProjects : List Types.Project.DeeplyResolved.DeeplyResolved
+    , dashboardStatistics : Types.Dashboard.Statistics.Statistics
     }
 
 
 selection : SelectionSet DeeplyResolved LondoGQL.Object.DeeplyResolvedDashboard
 selection =
-    SelectionSet.map2
+    SelectionSet.map3
         DeeplyResolved
         (LondoGQL.Object.DeeplyResolvedDashboard.dashboard Types.Dashboard.Dashboard.selection)
         (LondoGQL.Object.DeeplyResolvedDashboard.resolvedProjects Types.Project.DeeplyResolved.selection)
+        (LondoGQL.Object.DeeplyResolvedDashboard.dashboardStatistics Types.Dashboard.Statistics.selection)
 
 
 fetchWith :
