21b859ce256a4cecd559da018b65c2e0609d26d3
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-11-05 14:27:05 +0100

    Add math context definition function

--- app/utils/math/MathUtil.scala
+++ app/utils/math/MathUtil.scala
@@ -1,10 +1,16 @@
 package utils.math
 
 import cats.Order
+import math.Positive
+
+import java.math.{ MathContext, RoundingMode }
 
 object MathUtil {
 
   def clamp[N: Order](min: N, max: N)(value: N): N =
     spire.math.min(spire.math.max(value, min), max)
+
+  def mathContextBy(numberOfDecimalPlaces: Positive): MathContext =
+    new MathContext(numberOfDecimalPlaces.natural.toInt, RoundingMode.HALF_EVEN)
 
 }
