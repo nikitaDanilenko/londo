62d2462ebed88af842c2b030c8567ad548345cc4
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-11-08 10:19:08 +0100

    Use number of decimal places explicitly (math context seems to have another meaning)

--- app/utils/math/MathUtil.scala
+++ app/utils/math/MathUtil.scala
@@ -2,16 +2,18 @@ package utils.math
 
 import cats.Order
 import math.Positive
-import spire.math.Rational
+import spire.math.{ Rational, SafeLong }
 
-import java.math.RoundingMode
+import java.math.{ MathContext, RoundingMode }
 
 object MathUtil {
 
   def clamp[N: Order](min: N, max: N)(value: N): N =
     spire.math.min(spire.math.max(value, min), max)
 
-  def rationalToBigDecimal(numberOfDecimalPlaces: Positive): Rational => BigDecimal =
-    _.toBigDecimal(numberOfDecimalPlaces.natural.intValue, RoundingMode.HALF_EVEN)
+  def rationalToBigDecimal(numberOfDecimalPlaces: Positive): Rational => BigDecimal = { r =>
+    val denominator = SafeLong(10).pow(numberOfDecimalPlaces.natural.intValue)
+    r.roundTo(denominator).toBigDecimal(MathContext.DECIMAL128)
+  }
 
 }
