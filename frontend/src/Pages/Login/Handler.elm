de223eb7e79aa99a606b166d39e11ca75698e426
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-11-11 09:30:37 +0100

    Move functions to local scope

--- frontend/src/Pages/Login/Handler.elm
+++ frontend/src/Pages/Login/Handler.elm
@@ -8,7 +8,6 @@ import Pages.View.Tristate as Tristate
 import Ports
 import Result.Extra
 import Types.User.Login
-import Util.HttpUtil as HttpUtil
 
 
 init : Page.Flags -> ( Page.Model, Cmd Page.Msg )
@@ -25,54 +24,49 @@ update =
 
 updateLogic : Page.LogicMsg -> Page.Model -> ( Page.Model, Cmd Page.LogicMsg )
 updateLogic msg model =
+    let
+        setCredentials credentials =
+            ( model
+                |> Tristate.mapMain
+                    (Page.lenses.main.credentials.set credentials)
+            , Cmd.none
+            )
+
+        login =
+            ( model
+            , model
+                |> Tristate.foldMain Cmd.none
+                    (\main ->
+                        Types.User.Login.loginWith
+                            Page.GotResponse
+                            model.configuration
+                            main.credentials
+                    )
+            )
+
+        gotResponse remoteData =
+            remoteData
+                |> Result.Extra.unpack
+                    (\error ->
+                        ( Tristate.toError model error
+                        , Cmd.none
+                        )
+                    )
+                    (\token ->
+                        ( model
+                        , Cmd.batch
+                            [ Ports.storeToken token
+                            , Addresses.Frontend.overview.address () |> Links.frontendPage model.configuration |> Browser.Navigation.load
+                            ]
+                        )
+                    )
+    in
     case msg of
         Page.SetCredentials credentials ->
-            setCredentials model credentials
+            setCredentials credentials
 
         Page.Login ->
-            login model
+            login
 
         Page.GotResponse remoteData ->
-            gotResponse model remoteData
-
-
-setCredentials : Page.Model -> Types.User.Login.ClientInput -> ( Page.Model, Cmd Page.LogicMsg )
-setCredentials model credentials =
-    ( model
-        |> Tristate.mapMain
-            (Page.lenses.main.credentials.set credentials)
-    , Cmd.none
-    )
-
-
-login : Page.Model -> ( Page.Model, Cmd Page.LogicMsg )
-login model =
-    ( model
-    , model
-        |> Tristate.foldMain Cmd.none
-            (\main ->
-                Types.User.Login.loginWith
-                    Page.GotResponse
-                    model.configuration
-                    main.credentials
-            )
-    )
-
-
-gotResponse : Page.Model -> HttpUtil.GraphQLResult String -> ( Page.Model, Cmd Page.LogicMsg )
-gotResponse model remoteData =
-    remoteData
-        |> Result.Extra.unpack
-            (\error ->
-                ( Tristate.toError model error
-                , Cmd.none
-                )
-            )
-            (\token ->
-                ( model
-                , Cmd.batch
-                    [ Ports.storeToken token
-                    , Addresses.Frontend.overview.address () |> Links.frontendPage model.configuration |> Browser.Navigation.load
-                    ]
-                )
-            )
+            gotResponse remoteData
