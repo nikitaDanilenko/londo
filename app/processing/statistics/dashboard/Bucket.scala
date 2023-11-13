5ba9a78afb5daa3d0d2ad52d1568dc125c257768
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-11-13 21:34:01 +0100

    Use correct partitioning (facepalm)

--- app/processing/statistics/dashboard/Bucket.scala
+++ app/processing/statistics/dashboard/Bucket.scala
@@ -27,17 +27,17 @@ object Bucket extends Enum[Bucket] {
   case object Exactly100 extends Bucket
 
   private val intervals: List[(Interval[Rational], Bucket)] = List(
-    Interval.below(Rational(1, 10))                       -> Below10,
-    Interval.openUpper(Rational(1, 10), Rational(1, 20))  -> Below20,
-    Interval.openUpper(Rational(1, 20), Rational(1, 30))  -> Below30,
-    Interval.openUpper(Rational(1, 30), Rational(1, 40))  -> Below40,
-    Interval.openUpper(Rational(1, 40), Rational(1, 50))  -> Below50,
-    Interval.openUpper(Rational(1, 50), Rational(1, 60))  -> Below60,
-    Interval.openUpper(Rational(1, 60), Rational(1, 70))  -> Below70,
-    Interval.openUpper(Rational(1, 70), Rational(1, 80))  -> Below80,
-    Interval.openUpper(Rational(1, 80), Rational(1, 90))  -> Below90,
-    Interval.openUpper(Rational(1, 90), Rational(1, 100)) -> Below100,
-    Interval.atOrAbove(Rational(1))                       -> Exactly100
+    Interval.below(Rational(1, 10))                      -> Below10,
+    Interval.openUpper(Rational(1, 10), Rational(2, 10)) -> Below20,
+    Interval.openUpper(Rational(2, 10), Rational(3, 10)) -> Below30,
+    Interval.openUpper(Rational(3, 10), Rational(4, 10)) -> Below40,
+    Interval.openUpper(Rational(4, 10), Rational(5, 10)) -> Below50,
+    Interval.openUpper(Rational(5, 10), Rational(6, 10)) -> Below60,
+    Interval.openUpper(Rational(6, 10), Rational(7, 10)) -> Below70,
+    Interval.openUpper(Rational(7, 10), Rational(8, 10)) -> Below80,
+    Interval.openUpper(Rational(8, 10), Rational(9, 10)) -> Below90,
+    Interval.openUpper(Rational(9, 10), Rational(1))     -> Below100,
+    Interval.atOrAbove(Rational(1))                      -> Exactly100
   )
 
   def forProgress(progress: Progress): Bucket = {
